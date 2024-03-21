dfd0039a3b420253d9c0477ac320990c16e644a0
mark-torres10 <markptorres1@gmail.com>
2024-03-21 09:13:28 +0800

    first pass of post ranking service

---
+++ services/rank_posts/handler.py
@@ -0,0 +1,14 @@
+"""Manage ranking of posts. """
+from services.rank_posts.helper import main as rank_posts
+
+
+def main(event: dict, context: dict) -> int:
+    """Manage ranking and recommendation service. """
+    rank_posts(algorithm=event["algorithm"])
+    return 0
+
+
+if __name__ == "__main__":
+    event = {"algorithm": "reverse_chronological"}
+    context = {}
+    main(event=event, context=context)
---
+++ services/rank_posts/rank_posts.py
@@ -0,0 +1,34 @@
+"""Given a .csv of files, rank posts."""
+
+# inspired by https://github.com/twitter/the-algorithm-ml/blob/main/projects/home/recap/README.md
+LIKES_SCORE = 1
+RETWEETS_SCORE = 1.5
+COMMENTS_SCORE = 2
+
+
+def score_engagement(post: dict) -> int:
+    return (
+        LIKES_SCORE * post["Likes"]
+        + RETWEETS_SCORE * post["Retweets"]
+        + COMMENTS_SCORE * post["Comments"]
+    )
+
+
+def score_post(post: dict):
+    pass
+
+
+def score_posts(posts: list[dict]) -> list[float]:
+    pass
+
+
+def rank_posts(posts: list[dict]) -> list[dict]:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    pass
