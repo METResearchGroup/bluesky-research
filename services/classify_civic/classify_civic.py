517defe07abc914482901ab0d32bb7d76b4483dc
mark-torres10 <markptorres1@gmail.com>
2024-03-28 01:23:12 +0800

    start prompts and setup for classifying civic prompts

--- services/classify_civic/classify_civic.py
+++ services/classify_civic/classify_civic.py
@@ -1,6 +1,45 @@
 """Classifies if a post is civic or not."""
 THRESHOLD = 0.5
 
+BASE_PROMPT = """
+    Can you define if the content defined in <CONTENT> has "civic" content or not?
+
+    Use the following definition of "civic":
+    - 
+
+    Here are some examples of civic content:
+    {civic_examples}
+
+    Here are some examples of content that is not civic
+    {non_civic_examples}
+
+    <CONTENT>
+"""
+
+PROMPT_WITH_CONTEXT = """
+
+    The following in <CONTEXT> is provided as context for the content in
+    <CONTENT> in order to aid with classification:
+
+    <CONTEXT>
+    {context}
+"""
+
+PROMPT_GET_MOST_RELEVANT_CONTEXT = """
+    Given the content in <CONTENT> and the contexts in <CONTEXT>, please
+    provide the most relevant context.
+
+    Return the answer in JSON format.
+
+    Example output:
+    {relevant_context_output_example}
+"""
+
+def filter_relevant_context() -> str:
+    """Grab only the most relevant article, if any, for a given post."""
+    return ""
+
+
 def text_only_classification(text: str) -> tuple:
     return (None, None)
 
