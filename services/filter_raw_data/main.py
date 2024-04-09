1e3117e2766b5ff46a3b7122f7b0f5bf1993a362
mark-torres10 <markptorres1@gmail.com>
2024-04-09 09:00:50 +0800

    load testing filtering service

--- services/filter_raw_data/main.py
+++ services/filter_raw_data/main.py
@@ -3,7 +3,7 @@ from services.filter_raw_data.helper import filter_latest_raw_data
 
 def main() -> None:
     """Runs the main function."""
-    filter_latest_raw_data(1000)
+    filter_latest_raw_data(75000)
 
 
 if __name__ == "__main__":
