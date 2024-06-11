478c1d8fb330cdd03d3b3fec42a789663e4e5e77
mark-torres10 <markptorres1@gmail.com>
2024-06-11 18:56:17 -0500

    nit: move mutelist db objects

--- services/preprocess_raw_data/classify_spam/model.py
+++ services/preprocess_raw_data/classify_spam/model.py
@@ -3,7 +3,7 @@
 For now we'll use a placeholder until we come back and properly classify spam.
 """
 from services.consolidate_post_records.models import ConsolidatedPostRecordModel  # noqa
-from services.preprocess_raw_data.update_bluesky_mute_lists.database import get_muted_users_as_list_dicts  # noqa
+from lib.db.sql.muted_users_database import get_muted_users_as_list_dicts  # noqa
 
 muted_users: list[dict] = get_muted_users_as_list_dicts()
 muted_users_dids = set([user["did"] for user in muted_users])
