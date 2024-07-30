c7e8e458691313e3e70e2cbd74f4b479b870637c
mark-torres10 <markptorres1@gmail.com>
2024-07-29 23:05:35 -0400

    feat: update tf so that requests use proxy to forward /feed_api subpaths to the lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,12 +4,12 @@ provider "aws" {
 
 }
 
-# ECR repos
+### ECR repos ###
 resource "aws_ecr_repository" "feed_api_service" {
   name = "feed_api_service"
 }
 
-# Lambdas
+### Lambdas ###
 resource "aws_lambda_function" "bluesky_feed_api_lambda" {
   function_name = var.bsky_api_lambda_name
   role          = aws_iam_role.lambda_exec.arn
@@ -22,43 +22,56 @@ resource "aws_lambda_function" "bluesky_feed_api_lambda" {
   }
 }
 
-# API Gateway
+### API Gateway ###
+
+# define API Gateway REST API
 resource "aws_api_gateway_rest_api" "bluesky_feed_api_gateway" {
   name        = "bluesky_feed_api_gateway"
   description = "Bluesky Feed API"
 }
 
+# Define root resource for /feed_api
 resource "aws_api_gateway_resource" "bluesky_feed_api_gateway_root" {
   rest_api_id = aws_api_gateway_rest_api.bluesky_feed_api_gateway.id
   parent_id   = aws_api_gateway_rest_api.bluesky_feed_api_gateway.root_resource_id
   path_part   = "feed_api"
 }
 
-resource "aws_api_gateway_method" "bluesky_feed_api_gateway_method" {
+# define greedy path resource under /feed_api
+resource "aws_api_gateway_resource" "bluesky_feed_api_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.bluesky_feed_api_gateway.id
+  parent_id   = aws_api_gateway_resource.bluesky_feed_api_gateway_root.id
+  path_part   = "{proxy+}"
+}
+
+# define ANY method for /feed_api/{proxy+}
+resource "aws_api_gateway_method" "bluesky_feed_api_proxy_method" {
   rest_api_id   = aws_api_gateway_rest_api.bluesky_feed_api_gateway.id
-  resource_id   = aws_api_gateway_resource.bluesky_feed_api_gateway_root.id
-  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.bluesky_feed_api_proxy.id
+  http_method   = "ANY"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "bluesky_feed_api_gateway_integration" {
+# integrate ANY method with lambda, using AWS proxy to forward requests
+resource "aws_api_gateway_integration" "bluesky_feed_api_proxy_integration" {
   rest_api_id             = aws_api_gateway_rest_api.bluesky_feed_api_gateway.id
-  resource_id             = aws_api_gateway_resource.bluesky_feed_api_gateway_root.id
-  http_method             = aws_api_gateway_method.bluesky_feed_api_gateway_method.http_method
+  resource_id             = aws_api_gateway_resource.bluesky_feed_api_proxy.id
+  http_method             = aws_api_gateway_method.bluesky_feed_api_proxy_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.bluesky_feed_api_lambda.invoke_arn
 }
 
+# deploy API
 resource "aws_api_gateway_deployment" "bluesky_feed_api_gateway_deployment" {
   depends_on = [
-    aws_api_gateway_integration.bluesky_feed_api_gateway_integration
+    aws_api_gateway_integration.bluesky_feed_api_proxy_integration
   ]
 
   rest_api_id = aws_api_gateway_rest_api.bluesky_feed_api_gateway.id
   stage_name  = "prod"
-
 }
+
 
 # Lambda permission to allow API Gateway to invoke it
 resource "aws_lambda_permission" "api_gateway_permission" {
@@ -69,7 +82,7 @@ resource "aws_lambda_permission" "api_gateway_permission" {
   source_arn    = "${aws_api_gateway_rest_api.bluesky_feed_api_gateway.execution_arn}/*/*"
 }
 
-# Cloudwatch logging
+### Cloudwatch logging ###
 resource "aws_iam_role" "api_gateway_cloudwatch_role" {
   name = "APIGatewayCloudWatchLogsRole"
 
@@ -127,13 +140,14 @@ resource "aws_api_gateway_stage" "api_gateway_stage" {
   }
 
   depends_on = [aws_api_gateway_account.api_gateway_account]
-
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_cloudwatch_logs" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+### IAM Roles and Policies ###
 
 # Create IAM role for Lambda
 # https://spacelift.io/blog/terraform-aws-lambda
@@ -183,3 +197,33 @@ resource "aws_iam_role_policy_attachment" "lambda_attach_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = aws_iam_policy.lambda_s3_policy.arn
 }
+
+# S3 bucket policy, to allow read access for lambda.
+resource "aws_s3_bucket_policy" "bluesky_research_bucket_policy" {
+  bucket = var.s3_root_bucket_name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${aws_iam_role.lambda_exec.name}"
+        },
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:GetObjectVersion",
+          "s3:GetObjectAcl",
+          "s3:GetObjectTagging"
+        ],
+        Resource = [
+          "arn:aws:s3:::${var.s3_root_bucket_name}",
+          "arn:aws:s3:::${var.s3_root_bucket_name}/*"
+        ]
+      }
+    ]
+  })
+}
+
+data "aws_caller_identity" "current" {}
