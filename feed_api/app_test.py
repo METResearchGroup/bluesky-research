abc42aa87cf8076df93db3b95e2cf138b12d925a
mark-torres10 <markptorres1@gmail.com>
2024-08-03 18:34:54 -0400

    fix local test, verify that endpoint works

--- feed_api/app_test.py
+++ feed_api/app_test.py
@@ -4,43 +4,37 @@ import os
 import requests
 import base64
 
-root_url = "https://mindtechnologylab.com/manage_user"
-# root_url = "http://127.0.0.1:8000/"
+root_url = "https://mindtechnologylab.com/"
 endpoint = "manage_user"
 url = os.path.join(root_url, endpoint)
+
 # api_key = "your_secret_api_key"
 headers = {
     # "x-api-key": api_key,
     "Content-Type": "application/json"
 }
-test_profile_link = "https://bsky.app/profile/markptorres.bsky.social"
-operation = "modify"
-payload = {
-    "operation": operation,
-    "bluesky_user_profile_link": test_profile_link,
-    "test_user": False,
-    "condition": "engagement"
-}
+# test_profile_link = "https://bsky.app/profile/jbouie.bsky.social" # this user is very active.
+test_profile_link_2 = "https://bsky.app/profile/kilgoretrout.bsky.social"  # another active user.
+operation = "add"
 
 # get payloads
-
 add_user_payload = {
     "operation": "add",
-    "bluesky_user_profile_link": test_profile_link,
+    "bluesky_user_profile_link": test_profile_link_2,
     "test_user": False,
     "condition": "reverse_chronological"
 }
 
 modify_user_payload = {
     "operation": "modify",
-    "bluesky_user_profile_link": test_profile_link,
+    "bluesky_user_profile_link": test_profile_link_2,
     "test_user": False,
     "condition": "engagement"
 }
 
 delete_user_payload = {
     "operation": "delete",
-    "bluesky_user_profile_link": test_profile_link,
+    "bluesky_user_profile_link": test_profile_link_2,
     "test_user": False,
     "condition": "engagement"
 }
@@ -58,19 +52,23 @@ base64_delete_user_payload = base64.b64encode(
     str(delete_user_payload).encode("utf-8")
 ).decode("utf-8")
 
-print("Base64 add user payload:", base64_add_user_payload)
-print("Base64 modify user payload:", base64_modify_user_payload)
-print("Base64 delete user payload:", base64_delete_user_payload)
+# print("Base64 add user payload:", base64_add_user_payload)
+# print("Base64 modify user payload:", base64_modify_user_payload)
+# print("Base64 delete user payload:", base64_delete_user_payload)
 
-# response = requests.post(url, headers=headers, json=payload)
 current_time = datetime.datetime.now().strftime("%I:%M%p")
 print("Current time:", current_time)
-# response = requests.get(url, headers=headers) # test same endpoint but GET not POST
-# response = requests.get(
-#     "http://127.0.0.1:8000/test-get-s3",
-#     headers=headers
-# )
-# response = requests.post(url, headers=headers, json=payload)
 
-# print(response.status_code)
-# print(response.json())
+operation_to_payload_map = {
+    "add": base64_add_user_payload,
+    "modify": base64_modify_user_payload,
+    "delete": base64_delete_user_payload
+}
+
+response = requests.post(
+    url=url, headers=headers, json=add_user_payload
+)
+
+# breakpoint()
+print(response.status_code)
+print(response.json())
