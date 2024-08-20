70d9df2ae38387eafcda4413c2d03ca9b4ccc195
mark-torres10 <markptorres1@gmail.com>
2024-08-21 05:59:51 +0800

    start social network calculation

---
+++ services/get_existing_user_social_network/helper.py
@@ -0,0 +1,6 @@
+"""Get the existing social network for a given user."""
+
+
+def get_existing_user_social_network(user_did: str) -> list[str]:
+    """Get the existing social network for a given user."""
+    pass
