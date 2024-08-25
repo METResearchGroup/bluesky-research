4e63857362454b95fd757c3a2d4a9f3bb78a7518
mark-torres10 <markptorres1@gmail.com>
2024-08-25 22:03:43 +0800

    skeleton code for feed ranking and scoring

---
+++ services/rank_score_feeds/helper.py
@@ -0,0 +1,12 @@
+"""Helper functions for the rank_score_feeds service."""
+
+
+def do_rank_score_feeds():
+    # load users
+    # load consolidated enriched posts
+    # calculate scores for all the posts
+    # get lists of in-network and out-of-network posts
+    # create feeds for each user and sort by score
+    # map feeds to users
+    # write feeds to s3
+    pass
