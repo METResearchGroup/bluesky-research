478d68ed7177e08680d078d0ce2e1811ff88fb11
mark-torres10 <markptorres1@gmail.com>
2024-08-25 11:25:04 +0800

    create Glue tables on top of parquet files

--- pipelines/generate_vector_embeddings/handler.py
+++ pipelines/generate_vector_embeddings/handler.py
@@ -27,3 +27,18 @@ def lambda_handler(event, context):
 
 if __name__ == "__main__":
     lambda_handler(None, None)
+    # import pyarrow.parquet as pq
+    # import s3fs
+
+    # # Initialize S3 filesystem
+    # s3 = s3fs.S3FileSystem()
+
+    # # Path to your Parquet file
+    # parquet_file_path = 's3://bluesky-research/vector_embeddings/in_network_post_embeddings/2024-08-25-02:37:11.parquet'
+
+    # # Read the Parquet file
+    # parquet_file = pq.ParquetFile(s3.open(parquet_file_path))
+
+    # # Print the schema
+    # print(parquet_file.schema)
+    # breakpoint()
