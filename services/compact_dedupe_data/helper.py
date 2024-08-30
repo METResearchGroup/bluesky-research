3fdb38271a5f4576c6afa5cf70f8e5eda4f4e4b4
mark-torres10 <markptorres1@gmail.com>
2024-08-31 00:59:12 +0800

    v2 of compaction deduping lambda

--- services/compact_dedupe_data/helper.py
+++ services/compact_dedupe_data/helper.py
@@ -5,36 +5,39 @@ import os
 import pandas as pd
 
 from lib.aws.athena import Athena
+from lib.aws.glue import Glue
 from lib.aws.s3 import S3, ROOT_BUCKET
+from lib.helper import generate_current_datetime_str
 from lib.log.logger import get_logger
 from services.preprocess_raw_data.models import FilteredPreprocessedPostModel
 
 athena = Athena()
+glue = Glue()
 s3 = S3()
 
 logger = get_logger(__name__)
 
-
-def load_preprocessed_posts() -> list[FilteredPreprocessedPostModel]:
-    """Load the preprocessed posts from the database."""
-    query = """
-    SELECT * FROM `preprocessed_firehose_posts`
-    UNION ALL
-    SELECT * FROM `preprocessed_most_liked_posts`
-    """
-    df = athena.query_results_as_df(query=query)
-    df_dicts = df.to_dict(orient="records")
-    df_dicts = athena.parse_converted_pandas_dicts(df_dicts)
-    breakpoint()
-    df_dict_models = [FilteredPreprocessedPostModel(**df_dict) for df_dict in df_dicts]
-    return df_dict_models
+pd.set_option("display.max_columns", None)
+pd.set_option("max_colwidth", None)
 
 
 def export_preprocessed_posts(posts: list[FilteredPreprocessedPostModel]):
-    # TODO: add timestamp partition to the key, since this is what our
-    # Glue table expects.
-    # TODO: run crawler.
-    pass
+    """Exports the deduped and compacted preprocessed posts to S3."""
+    partition_key = S3.create_partition_key_based_on_timestamp(
+        timestamp_str=generate_current_datetime_str()
+    )
+    filename = "preprocessed_posts.jsonl"
+    full_key = os.path.join(
+        "preprocessed_data",
+        "post",
+        "compacted_deduped_data",
+        partition_key,
+        filename,
+    )  # noqa
+    post_dicts = [post.dict() for post in posts]
+    s3.write_dicts_jsonl_to_s3(data=post_dicts, key=full_key)
+    glue.start_crawler(crawler_name="preprocessed_posts_crawler")
+    logger.info(f"Exported deduped and compacted preprocessed posts to {full_key}")  # noqa
 
 
 def get_existing_keys() -> list[str]:
@@ -55,27 +58,30 @@ def delete_keys(keys: list[str]):
 
 
 def compact_dedupe_preprocessed_data():
-    posts = load_preprocessed_posts()
-    df = pd.DataFrame(posts)
-
+    posts: list[FilteredPreprocessedPostModel] = athena.get_latest_preprocessed_posts(
+        timestamp=None
+    )
+    post_dicts = [post.dict() for post in posts]
+    df = pd.DataFrame(post_dicts)
+    logger.info(f"Number of posts to compact: {len(df)}")
     # Sort by preprocessing_timestamp and dedupe by uri
     df = df.sort_values(by="preprocessing_timestamp", ascending=False).drop_duplicates(
         subset=["uri"]
     )
-
     df_dicts = df.to_dict(orient="records")
+    logger.info(f"Number of posts after dedupe: {len(df_dicts)}")
     df_dict_models = [FilteredPreprocessedPostModel(**df_dict) for df_dict in df_dicts]
-    breakpoint()
-
     # load existing keys from S3 (load before exporting new file so that
     # we have all the key names for the non-compacted files).
     existing_keys: list[str] = get_existing_keys()
+    logger.info(f"Number of existing keys: {len(existing_keys)}")
     # export new file to S3.
+    breakpoint()
     export_preprocessed_posts(df_dict_models)
     # drop existing keys from S3.
-    delete_keys(existing_keys)
+    # delete_keys(existing_keys) # TODO: verify that the approach works.
     logger.info("Successfully compacted dedupe preprocessed data")
 
 
 if __name__ == "__main__":
-    pass
+    compact_dedupe_preprocessed_data()
