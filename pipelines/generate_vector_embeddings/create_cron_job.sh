d946c169409b78dfd3ca0a339a2ec8abec2f6bac
mark-torres10 <markptorres1@gmail.com>
2024-09-01 16:26:30 +0800

    update slurm script

--- pipelines/generate_vector_embeddings/create_cron_job.sh
+++ pipelines/generate_vector_embeddings/create_cron_job.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #SBATCH -A jya0297
 #SBATCH -p gengpu
 #SBATCH --gres=gpu:a100:1
@@ -5,6 +7,9 @@
 #SBATCH -n 1
 #SBATCH -t 0:30:00
 #SBATCH --mem=10G
+#SBATCH --job-name=generate_vector_embeddings_job_jya0297_%j
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=markptorres1@gmail.com
 
 # NOTE: submit with `sbatch create_cron_job.sh`
 
@@ -15,5 +20,6 @@ CONDA_PATH="/hpc/software/mamba/23.1.0/etc/profile.d/conda.sh"
 PYTHONPATH="/projects/p32375/bluesky-research/:$PYTHONPATH"
 
 source $CONDA_PATH && conda activate bluesky_research && export PYTHONPATH=$PYTHONPATH
-
+echo "Starting slurm job."
 python handler.py
+echo "Completed slurm job."
