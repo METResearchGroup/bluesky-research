20a58fc4ef7d249becc2960fff575a0b825129b1
mark-torres10 <markptorres1@gmail.com>
2024-09-08 16:19:38 +0800

    update instructions from setup script

--- scripts/setup_new_firehose_ec2_instance.sh
+++ scripts/setup_new_firehose_ec2_instance.sh
@@ -25,32 +25,34 @@ ssh -i "firehoseSyncEc2Key.pem" ec2-user@ec2-3-129-70-136.us-east-2.compute.amaz
 # 3. Basic setup
 sudo yum update -y
 sudo yum install git -y
-sudo yum -y install docker
-sudo service docker start
+# sudo yum -y install docker
+# sudo service docker start
 
 # sudo service docker start
-sudo usermod -a -G docker ec2-user
+# sudo usermod -a -G docker ec2-user
 
 # Configure Docker log rotation. This is so that we don't accumulate a ton of
 # logs in the EC2 instance. We're not using a large instance, so we do need to
 # limit the amount of space we're using. The logs are stored in Cloudwatch, so
 # we don't need to worry about filling up the disk.
-sudo tee /etc/docker/daemon.json > /dev/null <<EOL
-{
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "10m",
-    "max-file": "3"
-  }
-}
-EOL
+# sudo tee /etc/docker/daemon.json > /dev/null <<EOL
+# {
+#   "log-driver": "json-file",
+#   "log-opts": {
+#     "max-size": "10m",
+#     "max-file": "3"
+#   }
+# }
+# EOL
 
 # Restart Docker to apply the new configuration
-sudo systemctl restart docker
+# sudo systemctl restart docker
 
 # exit instance, then SSH back in
 exit
-ssh -i "firehoseSyncEc2Key.pem" ec2-user@ec2-3-129-70-136.us-east-2.compute.amazonaws.com# install Github CLI: https://github.com/cli/cli/blob/trunk/docs/install_linux.md#amazon-linux-2-yum
+ssh -i "firehoseSyncEc2Key.pem" ec2-user@ec2-3-129-70-136.us-east-2.compute.amazonaws.com
+
+# install Github CLI: https://github.com/cli/cli/blob/trunk/docs/install_linux.md#amazon-linux-2-yum
 type -p yum-config-manager >/dev/null || sudo yum install yum-utils
 sudo yum-config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
 sudo yum install gh
@@ -61,34 +63,34 @@ gh auth login
 # clone repo
 git clone https://github.com/METResearchGroup/bluesky-research.git
 
-# add pythonpath
+# add pythonpath (ONLY if you're setting up a new instance)
 echo "export PYTHONPATH=/home/ec2-user/bluesky-research:$PYTHONPATH" >> ~/.bashrc && source ~/.bashrc
 
-# add default region
+# add default region (ONLY if you're setting up a new instance)
 echo 'export AWS_REGION="us-east-2"' >> ~/.bashrc && source ~/.bashrc
 
 # set up AWS access
 # aws configure # use the same creds as in .aws/config
 # aws configure sso # use the same creds as in .aws/config
 # export AWS_PROFILE="AWSAdministratorAccess-517478598677"
-ACCOUNT_ID=$(aws sts get-caller-identity --profile ${AWS_PROFILE} --query "Account" --output text)
+# ACCOUNT_ID=$(aws sts get-caller-identity --profile ${AWS_PROFILE} --query "Account" --output text)
 
 # connect to ECR
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.us-east-2.amazonaws.com
+# aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.us-east-2.amazonaws.com
 
 # get sync firehose image from ECR
-export FIREHOSE_REPO="sync_firehose_stream_service"
-docker pull ${ACCOUNT_ID}.dkr.ecr.us-east-2.amazonaws.com/${FIREHOSE_REPO}:latest
+# export FIREHOSE_REPO="sync_firehose_stream_service"
+# docker pull ${ACCOUNT_ID}.dkr.ecr.us-east-2.amazonaws.com/${FIREHOSE_REPO}:latest
 
 # run container (with the correct AWS credentials mounted)
-export CONTAINER_NAME="sync-firehose-container"
-export CONTAINER_ID=$(docker run -d --name ${CONTAINER_NAME} --platform linux/arm64 \
-     ${ACCOUNT_ID}.dkr.ecr.us-east-2.amazonaws.com/${FIREHOSE_REPO}:latest)
+# export CONTAINER_NAME="sync-firehose-container"
+# export CONTAINER_ID=$(docker run -d --name ${CONTAINER_NAME} --platform linux/arm64 \
+#      ${ACCOUNT_ID}.dkr.ecr.us-east-2.amazonaws.com/${FIREHOSE_REPO}:latest)
 
 # set up Cloudwatch logs for Docker container
 # aws logs create-log-group --log-group-name sync-firehose-logs # should already exist.
 
-# Install the CloudWatch agent
+# Install the CloudWatch agent (ONLY if you're setting up a new instance)
 sudo yum install -y amazon-cloudwatch-agent
 
 # Create the CloudWatch agent configuration file
@@ -119,6 +121,10 @@ EOL
 # Start the CloudWatch agent
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a start
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
+
+# run the firehose
+cd /home/ec2-user/bluesky-research/pipelines/sync_post_records/firehose
+nohup python firehose.py > output.log 2>&1 & tail -f output.log
 
 # Optional: stop and restart the CloudWatch agent
 # if you need to change the config or if you are restarting the Docker container
