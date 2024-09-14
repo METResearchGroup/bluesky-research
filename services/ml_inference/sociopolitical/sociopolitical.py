bd96fee441d69563893446528dceac00731e93cf
mark-torres10 <markptorres1@gmail.com>
2024-09-14 09:39:12 +0800

    increase post limit for sociopolitical inference

--- services/ml_inference/sociopolitical/sociopolitical.py
+++ services/ml_inference/sociopolitical/sociopolitical.py
@@ -33,7 +33,7 @@ LLM_MODEL_NAME = "GPT-4o mini"
 DEFAULT_BATCH_SIZE = 100
 DEFAULT_MINIBATCH_SIZE = 10
 # NOTE: will need to change as we make the sociopolitical lambda more efficient.
-max_num_posts = 800  # at current rate, it can handle ~100 posts/minute.
+max_num_posts = 1000  # given our batching, we can handle ~500 posts/minute.
 
 
 def generate_prompt(posts: list[FilteredPreprocessedPostModel]) -> str:
