c52e3211dbc3134c7cb93bf5d7cf59365888411c
mark-torres10 <markptorres1@gmail.com>
2024-09-16 12:51:52 +0800

    only show posts from the last 3 days

--- services/rank_score_feeds/helper.py
+++ services/rank_score_feeds/helper.py
@@ -28,7 +28,7 @@ from services.preprocess_raw_data.classify_language.model import classify
 from services.rank_score_feeds.models import CustomFeedModel
 
 max_feed_length = 50
-default_lookback_days = 5
+default_lookback_days = 3
 consolidated_enriched_posts_table_name = "consolidated_enriched_post_records"
 user_to_social_network_map_table_name = "user_social_networks"
 feeds_root_s3_key = "custom_feeds"
