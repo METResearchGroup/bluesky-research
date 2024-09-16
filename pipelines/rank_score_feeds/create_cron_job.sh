903ea3e7dcd7a5e0ac7a4be6cf8fef8403cf620c
mark-torres10 <markptorres1@gmail.com>
2024-09-16 13:41:54 +0800

    migrate consolidation and rank_score_feeds

---
+++ pipelines/rank_score_feeds/create_cron_job.sh
@@ -0,0 +1,15 @@
+# This bash script will create a cron job that runs the sbatch submit_job.sh every 6 hours.
+
+# Get the current directory
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# 4 hours, starting at the 45 minute mark
+CRON_EXPRESSION="45 */4 * * *"
+
+# Define the cron job command
+SBATCH_CRON_JOB="$CRON_EXPRESSION cd $DIR && sbatch submit_job.sh"
+
+# Add the cron job to the current user's crontab
+(crontab -l 2>/dev/null; echo "$SBATCH_CRON_JOB") | crontab -
+
+echo "Cron job created to run sbatch submit_job.sh every 4 hours, starting at the 45 minute mark."
