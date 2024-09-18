5e815cc5052cea129fce05a55e8b46332b44e8b6
mark-torres10 <markptorres1@gmail.com>
2024-09-18 17:57:21 +0800

    complete compaction

--- services/compact_all_services/constants.py
+++ services/compact_all_services/constants.py
@@ -1,10 +1,65 @@
 """Constants for compact_all_services."""
 
+import os
+
 MAP_SERVICE_TO_METADATA = {
-    "foo": {
-        "s3_prefix": "",
-        "glue_table_name": "",
+    # user_session_logs shouldn't be deduped, each row already is
+    # unique. Just need to compress.
+    "user_session_logs": {
+        "s3_prefix": "user_session_logs",
+        "glue_table_name": "user_session_logs",
         "primary_key": "",
         "timestamp_field": "",
-    }
+        "skip_deduping": True,
+    },
+    "feed_analytics": {
+        "s3_prefix": "feed_analytics",
+        "glue_table_name": "feed_generation_session_analytics",
+        "primary_key": "",
+        "timestamp_field": "",
+        "skip_deduping": True,
+    },
+    "post_scores": {
+        "s3_prefix": "post_scores",
+        "glue_table_name": "post_scores",
+        "primary_key": "uri",
+        "timestamp_field": "scored_timestamp",
+        "skip_deduping": False,
+    },
+    "consolidated_enriched_post_records": {
+        "s3_prefix": "consolidated_enriched_post_records",
+        "glue_table_name": "consolidated_enriched_post_records",
+        "primary_key": "uri",
+        "timestamp_field": "consolidation_timestamp",
+        "skip_deduping": False,
+    },
+    "ml_inference_perspective_api": {
+        "s3_prefix": "ml_inference_perspective_api",
+        "glue_table_name": "ml_inference_perspective_api",
+        "primary_key": "uri",
+        "timestamp_field": "label_timestamp",
+        "skip_deduping": False,
+    },
+    "ml_inference_sociopolitical": {
+        "s3_prefix": "ml_inference_sociopolitical",
+        "glue_table_name": "ml_inference_sociopolitical",
+        "primary_key": "uri",
+        "timestamp_field": "label_timestamp",
+        "skip_deduping": False,
+    },
+    "in_network_user_activity": {
+        "s3_prefix": os.path.join("in_network_user_activity", "create", "post"),
+        "glue_table_name": "in_network_firehose_sync_posts",
+        "primary_key": "uri",
+        "timestamp_field": "synctimestamp",
+        "skip_deduping": False,
+    },
+    # each row is a unique relationship, so no deduping required.
+    "scraped_user_social_network": {
+        "s3_prefix": "scraped-user-social-network",
+        "glue_table_name": "user_social_networks",
+        "primary_key": "",
+        "timestamp_field": "",
+        "skip_deduping": True,
+    },
 }
--- services/compact_all_services/helper.py
+++ services/compact_all_services/helper.py
@@ -6,6 +6,7 @@ import os
 from typing import Optional
 
 from lib.aws.athena import Athena
+from lib.aws.dynamodb import DynamoDB
 from lib.aws.s3 import ROOT_BUCKET, S3
 from lib.log.logger import get_logger
 from lib.helper import generate_current_datetime_str, track_performance
@@ -13,25 +14,43 @@ from services.compact_all_services.constants import MAP_SERVICE_TO_METADATA
 
 athena = Athena()
 s3 = S3()
+dynamodb = DynamoDB()
 logger = get_logger(__name__)
+
+# I can use the existing compaction sessions DynamoDB table, since it's still
+# technically a compaction session.
+dynamodb_table_name = "compaction_sessions"
 
 
 def generate_service_sql_query(service: str, timestamp: Optional[str] = None) -> str:
-    """Creates a SQL query to get the rows for a particular service."""
-    glue_table_name = MAP_SERVICE_TO_METADATA[service]["glue_table_name"]
-    primary_key = MAP_SERVICE_TO_METADATA[service]["primary_key"]
-    timestamp_field = MAP_SERVICE_TO_METADATA[service]["timestamp_field"]
-    timestamp_filter = f"AND {timestamp_field} >= '{timestamp}'" if timestamp else ""
-    query = f"""
-    SELECT *
-    FROM (
-        SELECT *,
-        ROW_NUMBER() OVER (PARTITION BY {primary_key} ORDER BY {timestamp_field} DESC) AS row_num
-        FROM {glue_table_name}
-    ) ranked
-    WHERE row_num = 1
-    {timestamp_filter}
+    """Creates a SQL query to get the rows for a particular service.
+
+    For some tables, we want to both compact files and dedupe them. For others,
+    like the user session logs, each row is already unique, so we just want to
+    compact the files.
     """
+    # NOTE: I use brackets instead of "get" since the impact of a silent "get"
+    # is quite high and could result in lost data.
+    skip_deduping = MAP_SERVICE_TO_METADATA[service]["skip_deduping"]
+    glue_table_name = MAP_SERVICE_TO_METADATA[service]["glue_table_name"]
+    if skip_deduping:
+        query = f"SELECT * FROM {glue_table_name}"
+    else:
+        primary_key = MAP_SERVICE_TO_METADATA[service]["primary_key"]
+        timestamp_field = MAP_SERVICE_TO_METADATA[service]["timestamp_field"]
+        timestamp_filter = (
+            f"AND {timestamp_field} >= '{timestamp}'" if timestamp else ""
+        )
+        query = f"""
+        SELECT *
+        FROM (
+            SELECT *,
+            ROW_NUMBER() OVER (PARTITION BY {primary_key} ORDER BY {timestamp_field} DESC) AS row_num
+            FROM {glue_table_name}
+        ) ranked
+        WHERE row_num = 1
+        {timestamp_filter}
+        """
     return query
 
 
@@ -40,22 +59,36 @@ def generate_service_sql_query(service: str, timestamp: Optional[str] = None) ->
 # (and add an option to include them, for when I want to compact all files),
 # including all previously compacted files.
 def get_service_compaction_session(service: str) -> dict:
-    pass
+    return {}
 
 
-def insert_service_compaction_session(service: str, compaction_session: dict) -> None:
-    pass
+def insert_service_compaction_session(compaction_session: dict) -> None:
+    try:
+        dynamodb.insert_item_into_table(
+            item=compaction_session, table_name=dynamodb_table_name
+        )
+        logger.info(
+            f"Successfully inserted compaction session: {compaction_session}"  # noqa
+        )  # noqa
+    except Exception as e:
+        logger.error(f"Failed to insert compaction session: {e}")  # noqa
+        raise
 
 
+# NOTE: in the future, if the data is too large, I can write multiple files
+# instead of 1. I can just split it and then for each batch, I can do
+# something like f"{filename}_{i}.jsonl" where i is the batch number.
 def export_compacted_data(service: str, data: list[dict]) -> None:
     """Exports compacted data to S3.
 
     The compacted data is exported to a "compacted" subdirectory of the
     service's S3 prefix.
     """
-    s3_prefix = ""  # TODO: get from constants
-    full_s3_prefix = os.path.join(s3_prefix, "compacted")
-    s3.write_dicts_jsonl_to_s3(data=data, s3_prefix=full_s3_prefix)
+    s3_prefix = MAP_SERVICE_TO_METADATA[service]["s3_prefix"]
+    timestamp = generate_current_datetime_str()
+    filename = f"{timestamp}.jsonl"
+    full_s3_prefix = os.path.join(s3_prefix, "compacted", filename)
+    s3.write_dicts_jsonl_to_s3(data=data, key=full_s3_prefix)
     logger.info(
         f"Successfully inserted {len(data)} compacted posts for service '{service}' at {full_s3_prefix}"
     )  # noqa
@@ -99,34 +132,48 @@ def compact_single_service(service: str) -> None:
     # TODO: make this flexible so that I can either compact all data after a certain
     # timestamp or compact all the data after all time.
     latest_service_compaction_session: dict = get_service_compaction_session(service)
-    timestamp = latest_service_compaction_session["compaction_timestamp"]
-    print(f"Compacting data from {service} after {timestamp}")
+    timestamp = latest_service_compaction_session.get("compaction_timestamp", None)
+    if timestamp:
+        print(f"Compacting data from {service} after {timestamp}")
     query = generate_service_sql_query(service, timestamp)
     existing_keys: list[str] = get_existing_keys(service)
+    if existing_keys:
+        logger.info(f"Compacting {len(existing_keys)} files for {service}")
+    else:
+        logger.info(
+            f"No files to compact for {service}. Only files, if any, are already compacted"
+        )  # noqa
+        return
     df = athena.query_results_as_df(query)
     df_dicts = df.to_dict(orient="records")
     df_dicts = athena.parse_converted_pandas_dicts(df_dicts)
     logger.info(f"Successfully compacted data for {service}")
-    # TODO: add more fields for compaction session, like # of posts before and
-    # after, # of files deleted, etc.
-    # TODO: should I also do deduping here as well? Maybe? In that case, I
-    # need the PK of each table (probably URI tbh). Maybe instead of doing a
-    # bunch of map constants, I can do a single service map where each key
-    # is the service name and each value is a dict with the service name,
-    # the query, the S3 prefix, and the PK
     export_compacted_data(service=service, data=df_dicts)
     delete_keys(existing_keys)
     compaction_session = {
         "compaction_timestamp": generate_current_datetime_str(),
+        "service": service,
+        "num_rows_after_compaction": len(df_dicts),
+        "num_files_compacted": len(existing_keys),
+        "num_files_deleted": len(existing_keys),
     }
-    insert_service_compaction_session(
-        service=service, compaction_session=compaction_session
-    )
-
-
-def compact_all_services() -> None:
-    pass
+    insert_service_compaction_session(compaction_session=compaction_session)
 
 
 def do_compact_services() -> None:
-    pass
+    services = [
+        "user_session_logs",
+        "feed_analytics",
+        "post_scores",
+        "consolidated_enriched_post_records",
+        "ml_inference_perspective_api",
+        "ml_inference_sociopolitical",
+        # "in_network_user_activity",
+        "scraped_user_social_network",
+    ]
+    for service in services:
+        compact_single_service(service)
+
+
+if __name__ == "__main__":
+    do_compact_services()
