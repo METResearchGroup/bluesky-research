7586637dee9a09b2a9deabf2b4bc3cf39ea1b777
mark-torres10 <markptorres1@gmail.com>
2024-09-18 08:35:49 +0800

    start compaction service

---
+++ services/compact_all_services/helper.py
@@ -0,0 +1,13 @@
+MAP_SERVICE_TO_S3_PREFIX = {}
+
+
+def compact_single_service(service: str) -> None:
+    pass
+
+
+def compact_all_services() -> None:
+    pass
+
+
+def do_compact_services() -> None:
+    pass
