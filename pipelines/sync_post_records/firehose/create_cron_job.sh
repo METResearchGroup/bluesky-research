52c5c0d8e59c11cd246f8b3d12994fe450f761f0
mark-torres10 <markptorres1@gmail.com>
2024-09-26 15:50:16 +0800

    trigger firehose cron job at 8am utc (3am Central)

--- pipelines/sync_post_records/firehose/create_cron_job.sh
+++ pipelines/sync_post_records/firehose/create_cron_job.sh
@@ -3,8 +3,8 @@
 # Get the current directory
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Every day at 12am UTC
-CRON_EXPRESSION="0 0 * * *"
+# Every day at 8am UTC
+CRON_EXPRESSION="0 8 * * *"
 
 # Define the cron job command
 SBATCH_CRON_JOB="$CRON_EXPRESSION cd $DIR && sbatch submit_job.sh"
@@ -12,4 +12,4 @@ SBATCH_CRON_JOB="$CRON_EXPRESSION cd $DIR && sbatch submit_job.sh"
 # Add the cron job to the current user's crontab
 (crontab -l 2>/dev/null; echo "$SBATCH_CRON_JOB") | crontab -
 
-echo "Cron job created to run sbatch submit_job.sh every day at 12am UTC."
+echo "Cron job created to run sbatch submit_job.sh every day at 8am UTC."
