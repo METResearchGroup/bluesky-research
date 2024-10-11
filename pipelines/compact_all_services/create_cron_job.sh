adc00200b49188dfab69e9cc7e772ff69c5d10e8
mark-torres10 <markptorres1@gmail.com>
2024-10-11 00:01:31 -0500

    run compaction 2x a day

--- pipelines/compact_all_services/create_cron_job.sh
+++ pipelines/compact_all_services/create_cron_job.sh
@@ -1,10 +1,10 @@
-# This bash script will create a cron job that runs the sbatch submit_job.sh every 6 hours.
+# This bash script will create a cron job that runs the sbatch submit_job.sh twice per day, starting at 2am Central Time (7am UTC).
 
 # Get the current directory
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Run once per day at 2am Central Time (7am UTC)
-CRON_EXPRESSION="0 7 * * *"
+# Run twice per day at 2am and 2pm Central Time (7am and 7pm UTC)
+CRON_EXPRESSION="0 7,19 * * *"
 
 # Define the cron job command
 SBATCH_CRON_JOB="$CRON_EXPRESSION cd $DIR && sbatch submit_job.sh"
@@ -12,4 +12,4 @@ SBATCH_CRON_JOB="$CRON_EXPRESSION cd $DIR && sbatch submit_job.sh"
 # Add the cron job to the current user's crontab
 (crontab -l 2>/dev/null; echo "$SBATCH_CRON_JOB") | crontab -
 
-echo "Cron job created to run sbatch submit_job.sh once per day at 2am Central Time (7am UTC)."
+echo "Cron job created to run sbatch submit_job.sh twice per day at 2am and 2pm Central Time (7am and 7pm UTC)."
