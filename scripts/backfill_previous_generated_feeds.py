7bf495c31a0f82530fb78194cbdaf5367c7e39ac
mark-torres10 <markptorres1@gmail.com>
2024-11-10 14:31:30 +0800

    Complete the backfill script

--- scripts/backfill_previous_generated_feeds.py
+++ scripts/backfill_previous_generated_feeds.py
@@ -1,10 +1,4 @@
-"""Processes previous feeds and:
-
-1. Adds a 'feed_id' to each feed.
-2. Dumps the 'feed' field as JSON-dumped files to S3 (instead of the complex
-struct that it currently is).
-    - Requires that the existing 'cached_custom_feeds' table is changed to
-    have the 'feed' field be a string and not a complex struct.
+"""Processes previous feeds and adds a 'feed_id' to each feed.
 
 This needs to be done on the "cached" feeds in order to prove that it works,
 and then can be done on the "active" feeds once that's verified.
@@ -23,52 +17,36 @@ It looks like you can't change the name of the table created by the Glue crawler
 so I need to change the S3 paths to "cached_custom_feeds".
 """
 
-import json
-import re
+from datetime import datetime, timedelta
+import os
 
 import pandas as pd
 
 from lib.aws.athena import Athena
+from lib.aws.s3 import S3
 
 athena = Athena()
+s3 = S3()
+
+start_date = "2024-10-01"
+start_timestamp = datetime.strptime(start_date, "%Y-%m-%d")
 
 
-def parse_feed_string(feed_string: str) -> list[dict]:
-    """Parses the feed string.
-
-    e.g.,
-    >> parse_feed_string('[{item=at://did:plc:pmyqirafcp3jqdhrl7crpq7t/app.bsky.feed.post/3kzoxsoyom62q, score=2.8175526}, {item=at://did:plc:eg336dt7kyike5xkyed3iwcv/app.bsky.feed.post/3kzoptflyq72h, score=2.8040724}, {item=at://did:plc:fxr5qi344wulv4ggowdzv57y/app.bsky.feed.post/3kzo2exfi5z2r, score=2.8030896}]')
-    [{'item': 'at://did:plc:pmyqirafcp3jqdhrl7crpq7t/app.bsky.feed.post/3kzoxsoyom62q', 'score': 2.8175526}, {'item': 'at://did:plc:eg336dt7kyike5xkyed3iwcv/app.bsky.feed.post/3kzoptflyq72h', 'score': 2.8040724}, {'item': 'at://did:plc:fxr5qi344wulv4ggowdzv57y/app.bsky.feed.post/3kzo2exfi5z2r', 'score': 2.8030896}]
-    """  # noqa
-    # Step 1: Surround keys with quotes
-    feed_string = re.sub(r"(\w+)=", r'"\1":', feed_string)
-
-    # Step 2: Surround the
-    # did:plc:<some string>/app.bsky.feed.post/<some string> with quotes
-    # at://did:<any string>/app.bsky.feed.post/<some string> with quotes
-    feed_string = re.sub(
-        # r"(at://did:plc:[\w]+/app\.bsky\.feed\.post/\w+)", r'"\1"', feed_string
-        r"(at://did:[^/]+/app\.bsky\.feed\.post/\w+)",
-        r'"\1"',
-        feed_string,
-    )
-
-    # Step 3: Parse the string into a list of dicts
-    feed = json.loads(feed_string)
-    return feed
+def get_partition_dates() -> list[str]:
+    partition_dates = []
+    current_date = start_timestamp
+    while current_date < datetime.now():
+        partition_dates.append(current_date.strftime("%Y-%m-%d"))
+        current_date += timedelta(days=1)
+    return partition_dates
 
 
-def load_cached_feeds_as_df() -> pd.DataFrame:
-    query = """SELECT * FROM cached_custom_feeds"""
+def load_cached_feeds_as_df(partition_date: str) -> pd.DataFrame:
+    query = f"""SELECT * FROM cached_custom_feeds WHERE partition_date = '{partition_date}'"""
     df = athena.query_results_as_df(query)
     df_dicts = df.to_dict(orient="records")
     df_dicts = athena.parse_converted_pandas_dicts(df_dicts)
-    feeds = [row["feed"] for row in df_dicts]
-    user_feed_dicts: list[list[dict]] = [parse_feed_string(feed) for feed in feeds]
-
     df = pd.DataFrame(df_dicts)
-    df["feed"] = user_feed_dicts
-
     return df
 
 
@@ -79,11 +57,32 @@ def add_feed_id_col_to_df(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def upsert_df_to_cached_feeds(df: pd.DataFrame) -> None:
-    pass
+def upsert_df_to_cached_feeds(df: pd.DataFrame, partition_date: str) -> None:
+    filename = f"backfilled_cached_custom_feeds_{partition_date}.jsonl"
+    key = os.path.join(
+        "custom_feeds",
+        "cached_custom_feeds",
+        f"partition_date={partition_date}",
+        filename,
+    )
+    df_dicts = df.to_dict(orient="records")
+    s3.write_dicts_jsonl_to_s3(data=df_dicts, key=key)
 
 
 def main():
-    df = load_cached_feeds_as_df()
-    df = add_feed_id_col_to_df(df)
-    upsert_df_to_cached_feeds(df)
+    partition_dates = get_partition_dates()
+    finished_dates = []
+    for partition_date in partition_dates:
+        if partition_date in finished_dates:
+            print(f"Skipping {partition_date} since it's already done.")
+            continue
+        print(f"Processing {partition_date}...")
+        df = load_cached_feeds_as_df(partition_date)
+        df = add_feed_id_col_to_df(df)
+        upsert_df_to_cached_feeds(df=df, partition_date=partition_date)
+        print(f"Done processing {partition_date}!")
+    print("Done processing all dates.")
+
+
+if __name__ == "__main__":
+    main()
