e32393344ef5cc076006fb6a9abb15128306b360
mark-torres10 <markptorres1@gmail.com>
2024-12-03 14:12:10 -0600

    Consolidate posts that are in the feeds

---
+++ scripts/analytics/get_consolidated_posts_in_feeds.py
@@ -0,0 +1,13 @@
+"""Gets the consolidated and enriched posts that ended up in the feeds."""
+
+analytics_data_path = "/projects/p32375/bluesky_research_data/analytics"
+
+# get partition dates
+
+# loop through each partition date
+
+# load the data
+
+# get the posts that were in the feeds
+
+# export those to analytics/consolidated_posts/partition_date={partition_date}
