86d085774ae643439a4790d8622e39e14a9c169c
mark-torres10 <markptorres1@gmail.com>
2024-12-03 15:41:12 -0500

    wip

--- scripts/analytics/get_consolidated_posts_in_feeds.py
+++ scripts/analytics/get_consolidated_posts_in_feeds.py
@@ -1,5 +1,7 @@
 """Gets the consolidated and enriched posts that ended up in the feeds."""
 
+from lib.db.manage_local_data import load_data_from_local_storage
+
 analytics_data_path = "/projects/p32375/bluesky_research_data/analytics"
 
 # get partition dates
@@ -11,3 +13,21 @@ analytics_data_path = "/projects/p32375/bluesky_research_data/analytics"
 # get the posts that were in the feeds
 
 # export those to analytics/consolidated_posts/partition_date={partition_date}
+
+
+def main():
+    # get the partition dates to load posts from. This should be a few days
+    # before the earliest feeds since it'll take posts from the previous days.
+    # posts_partition_dates = get_partition_dates(
+    #     start_date="2024-09-26", end_date="2024-12-01"
+    # )
+    consolidated_enriched_posts_df = load_data_from_local_storage(
+        service="consolidated_enriched_post_records",
+        directory="cache",
+    )
+    print(f"Loaded {len(consolidated_enriched_posts_df)} consolidated enriched posts")
+    breakpoint()
+
+
+if __name__ == "__main__":
+    main()
