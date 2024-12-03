a65d4aff53c853d223eddbd4130b642c46833533
mark-torres10 <markptorres1@gmail.com>
2024-12-02 20:56:10 -0500

    Get all feeds and user session logs and aggregate them locally

---
+++ scripts/analytics/consolidate_user_session_logs.py
@@ -0,0 +1,67 @@
+"""Loads all the user session logs that were dumped locally and consolidates
+them into a single parquet file.
+"""
+
+from datetime import datetime, timedelta
+import json
+import os
+
+import numpy as np
+import pandas as pd
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+log_filedir = os.path.join(current_dir, "user_session_logs")
+
+
+def get_partition_dates(start_date: str, end_date: str) -> list[str]:
+    """Returns a list of dates between start_date and end_date, inclusive.
+
+    Args:
+        start_date: Start date in YYYY-MM-DD format
+        end_date: End date in YYYY-MM-DD format
+
+    Returns:
+        List of dates in YYYY-MM-DD format
+    """
+    partition_dates = []
+    current_date = datetime.strptime(start_date, "%Y-%m-%d")
+    end_timestamp = datetime.strptime(end_date, "%Y-%m-%d")
+
+    while current_date <= end_timestamp:
+        partition_dates.append(current_date.strftime("%Y-%m-%d"))
+        current_date += timedelta(days=1)
+
+    return partition_dates
+
+
+def serialize_feed(feed):
+    if isinstance(feed, np.ndarray):
+        return json.dumps(feed.tolist())
+    elif isinstance(feed, (list, dict)):
+        return json.dumps(feed)
+    return json.dumps(feed) if feed is not None else None
+
+
+def main():
+    partition_dates = get_partition_dates(
+        start_date="2024-09-30", end_date="2024-12-01"
+    )
+    dfs: list[pd.DataFrame] = []
+    for partition_date in partition_dates:
+        filepath = os.path.join(log_filedir, f"{partition_date}.parquet")
+        try:
+            df = pd.read_parquet(filepath)
+        except FileNotFoundError:
+            print(f"File not found: {filepath}")
+            continue
+        dfs.append(df)
+    compacted_df = pd.concat(dfs)
+    feeds_df = pd.DataFrame({"feed": compacted_df["feed"].apply(serialize_feed)})
+    compacted_df["feed"] = feeds_df["feed"]
+    compacted_df.to_parquet(
+        os.path.join(current_dir, "consolidated_user_session_logs.parquet")
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/analytics/get_all_user_session_logs.py
@@ -0,0 +1,77 @@
+"""Fetches all the user session logs from S3."""
+
+from datetime import datetime, timedelta
+import os
+
+from lib.aws.s3 import S3
+
+s3 = S3()
+
+root_s3_uri = "user_session_logs"
+
+exclude_partition_date = "2024-10-13"  # server crashed on this date.
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_partition_dates(start_date: str, end_date: str) -> list[str]:
+    """Returns a list of dates between start_date and end_date, inclusive.
+
+    Args:
+        start_date: Start date in YYYY-MM-DD format
+        end_date: End date in YYYY-MM-DD format
+
+    Returns:
+        List of dates in YYYY-MM-DD format
+    """
+    partition_dates = []
+    current_date = datetime.strptime(start_date, "%Y-%m-%d")
+    end_timestamp = datetime.strptime(end_date, "%Y-%m-%d")
+
+    while current_date <= end_timestamp:
+        if current_date.strftime("%Y-%m-%d") == exclude_partition_date:
+            current_date += timedelta(days=1)
+            continue
+        partition_dates.append(current_date.strftime("%Y-%m-%d"))
+        current_date += timedelta(days=1)
+
+    return partition_dates
+
+
+def main():
+    print("Fetching all the user session logs...")
+    partition_dates: list[str] = get_partition_dates(
+        start_date="2024-09-30", end_date="2024-12-01"
+    )
+    for partition_date in partition_dates:
+        if partition_date == exclude_partition_date:
+            continue
+        print(f"Fetching {partition_date}...")
+        folder_prefix = os.path.join(root_s3_uri, f"partition_date={partition_date}")
+        print(f"Folder prefix: {folder_prefix}")
+        keys = s3.get_keys_given_prefix(folder_prefix)
+        print(f"Found {len(keys)} keys for {partition_date}...")
+        compacted_keys = [key for key in keys if "compacted" in key]
+
+        # if there is a compacted version of the keys, it's already compacted,
+        # else download all the keys individually.
+        if compacted_keys:
+            print(f"Found {len(compacted_keys)} compacted keys for {partition_date}...")
+            df = s3.load_keys_to_df(compacted_keys)
+        else:
+            print(
+                f"No compacted keys found for {partition_date}. Loading {len(keys)} keys..."
+            )
+            df = s3.load_keys_to_df(keys)
+        export_filepath = os.path.join(
+            current_dir, "user_session_logs", f"{partition_date}.parquet"
+        )
+        df.to_parquet(export_filepath)
+        print(f"Saved to {export_filepath}")
+        print(f"Finished fetching {partition_date}.")
+        print("-" * 10)
+    print("Finished fetching all the user session logs.")
+
+
+if __name__ == "__main__":
+    main()
