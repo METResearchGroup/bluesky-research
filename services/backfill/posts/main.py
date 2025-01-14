691ea980470fccbb8d9bc654d16a7d2b27ea541b
mark-torres10 <markptorres1@gmail.com>
2025-01-14 16:23:26 -0500

    Continue backfilling service coordination

--- services/backfill/posts/main.py
+++ services/backfill/posts/main.py
@@ -1,11 +1,19 @@
 """Service for backfilling posts."""
 
+from lib.db.queue import Queue
+from lib.log.logger import get_logger
 from services.backfill.posts.load_data import load_posts_to_backfill
+
+logger = get_logger(__name__)
 
 
 def backfill_posts(payload: dict):
     posts_to_backfill: dict[str, set[str]] = load_posts_to_backfill(
         payload.get("integration")
     )
+    context = {"total": len(posts_to_backfill)}
+    logger.info(f"Loaded {len(posts_to_backfill)} posts to backfill.", context=context)
     for integration, post_uris in posts_to_backfill.items():
-        pass
+        queue = Queue(queue_name=integration, create_new_queue=True)
+        payloads = [{"uri": uri} for uri in post_uris]
+        queue.batch_add_item_to_queue(payloads)
