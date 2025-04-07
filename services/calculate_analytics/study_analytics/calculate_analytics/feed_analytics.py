0383eced13ade7e15ae19e762275a03e049c5ffe
mark-torres10 <markptorres1@gmail.com>
2025-04-07 21:47:15 +0900

    Add script to calculate average labels

--- services/calculate_analytics/study_analytics/calculate_analytics/feed_analytics.py
+++ services/calculate_analytics/study_analytics/calculate_analytics/feed_analytics.py
@@ -1,5 +1,8 @@
 """Calculates the average feed content for each user."""
 
+from typing import Optional
+
+import numpy as np
 import pandas as pd
 
 from lib.log.logger import get_logger
@@ -15,6 +18,7 @@ from services.calculate_analytics.study_analytics.load_data.load_data import (
 start_date_inclusive = wave_1_study_start_date_inclusive
 end_date_inclusive = wave_2_study_end_date_inclusive  # 2024-12-01 (inclusive)
 exclude_partition_dates = ["2024-10-08"]
+default_label_threshold = 0.5
 
 logger = get_logger(__file__)
 
@@ -135,6 +139,372 @@ def get_per_user_feed_averages_for_study(
         logger.info(f"Getting per-user averages for partition date: {partition_date}")
         df = get_per_user_feed_averages_for_partition_date(
             partition_date=partition_date, load_unfiltered_posts=load_unfiltered_posts
+        )
+        df["date"] = partition_date
+        dfs.append(df)
+    concat_df = pd.concat(dfs)
+    # Sort by user and partition date in ascending order
+    concat_df = concat_df.sort_values(["user", "date"], ascending=[True, True])
+    logger.info(
+        f"Exporting a dataframe of per-user feed averages, with {len(concat_df)} rows"
+    )
+    return concat_df
+
+
+def get_per_user_feed_average_labels_for_partition_date(
+    partition_date: str,
+    load_unfiltered_posts: bool = True,
+    threshold: float = default_label_threshold,
+):
+    """For each user, calculates the average labels for the feed content on a
+    given partition date.
+
+    Unlike `get_per_user_feed_averages_for_partition_date`, which calculates,
+    for example, the average % of toxicity of the posts that appeared
+    in the user's feed on the given date, we calculate the average % of toxic
+    posts (here, using a default threshold of p=0.5 for our labels).
+
+    We use a cutoff of p=0.5 by default for all attributes that return a
+    probability. For the LLM labels, we return the average label (which
+    is also how we do it in `get_per_user_feed_averages_for_partition_date`).
+    """
+    map_user_to_posts_df: dict[str, pd.DataFrame] = get_hydrated_feed_posts_per_user(
+        partition_date=partition_date, load_unfiltered_posts=load_unfiltered_posts
+    )
+
+    # Create list to store per-user proportions
+    user_proportions = []
+
+    logger.info(
+        f"Calculating per-user average labels for partition date {partition_date}"
+    )
+
+    for user, posts_df in map_user_to_posts_df.items():
+        # Calculate proportion for each feature
+        # here, the average is the proportion of posts, out of all posts shown
+        # to the user on the given day, that have the given label.
+        proportions = {
+            "user": user,
+            "user_did": user,
+            "prop_toxic_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_toxic"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_severe_toxic_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_severe_toxic"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_identity_attack_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_identity_attack"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_insult_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_insult"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_profanity_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_profanity"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_threat_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_threat"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_affinity_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_affinity"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_compassion_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_compassion"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_constructive_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_constructive"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_curiosity_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_curiosity"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_nuance_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_nuance"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_personal_story_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_personal_story"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_reasoning_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_reasoning"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_respect_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_respect"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_alienation_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_alienation"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_fearmongering_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_fearmongering"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_generalization_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_generalization"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_moral_outrage_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_moral_outrage"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_scapegoating_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_scapegoating"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_sexually_explicit_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_sexually_explicit"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_flirtation_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_flirtation"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_spam_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_spam"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_emotion_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_emotion"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_intergroup_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_intergroup"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_moral_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_moral"].dropna()
+                        ]
+                    )
+                )
+            ),
+            "prop_ime_other_posts": (
+                np.mean(
+                    np.array(
+                        [
+                            1 if prob >= threshold else 0
+                            for prob in posts_df["prob_other"].dropna()
+                        ]
+                    )
+                )
+            ),
+        }
+
+        # Calculate political averages
+        total_rows = len(posts_df)
+        avg_is_political = posts_df["is_sociopolitical"].dropna().mean()
+
+        political_proportions = {
+            "prop_is_political": avg_is_political,
+            "prop_is_not_political": 1 - avg_is_political,
+            "prop_is_political_left": (
+                posts_df["political_ideology_label"].fillna("").eq("left")
+            ).sum()
+            / total_rows,
+            "prop_is_political_right": (
+                posts_df["political_ideology_label"].fillna("").eq("right")
+            ).sum()
+            / total_rows,
+            "prop_is_political_moderate": (
+                posts_df["political_ideology_label"].fillna("").eq("moderate")
+            ).sum()
+            / total_rows,
+            "prop_is_political_unclear": (
+                posts_df["political_ideology_label"].fillna("").eq("unclear")
+            ).sum()
+            / total_rows,
+        }
+
+        # Combine all averages
+        proportions.update(political_proportions)
+
+        user_proportions.append(proportions)
+
+    # Convert to dataframe
+    proportions_df = pd.DataFrame(user_proportions)
+    proportions_df = proportions_df.set_index("user")
+
+    logger.info(
+        f"Finished calculating per-user label proportions for partition date {partition_date}"
+    )
+
+    return proportions_df
+
+
+def get_per_user_feed_average_labels_for_study(
+    load_unfiltered_posts: bool = True,
+    threshold: Optional[float] = default_label_threshold,
+):
+    dfs: list[pd.DataFrame] = []
+    partition_dates: list[str] = get_partition_dates(
+        start_date=start_date_inclusive,
+        end_date=end_date_inclusive,
+        exclude_partition_dates=exclude_partition_dates,
+    )
+    for partition_date in partition_dates:
+        logger.info(f"Getting per-user averages for partition date: {partition_date}")
+        df = get_per_user_feed_average_labels_for_partition_date(
+            partition_date=partition_date,
+            load_unfiltered_posts=load_unfiltered_posts,
+            threshold=threshold,
         )
         df["date"] = partition_date
         dfs.append(df)
