7e8e36a7e4911f29af8e5448a5de8f988c703760
mark-torres10 <markptorres1@gmail.com>
2025-05-04 16:21:02 +0800

    Refactor YAML logic

--- api/backfill_router/scripts/trigger_backfill_study_users/query_pds.py
+++ api/backfill_router/scripts/trigger_backfill_study_users/query_pds.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     config_path = (
         sys.argv[1]
         if len(sys.argv) > 1
-        else "api/backfill_router/config/examples/backfill_study_users.yaml"
+        # else "api/backfill_router/config/examples/backfill_study_users.yaml"
+        else "api/backfill_router/config/examples/backfill_posts_engaged_with_by_study_users.yaml"
     )
     run_pds_backfill_api(config_path)
