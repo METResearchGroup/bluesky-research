ad490afc2a4c7e621f8214c4ccd3a399b921f210
mark-torres10 <markptorres1@gmail.com>
2025-05-05 21:05:34 +0800

    Update 'preprocess_raw_data' handler to be like the other integration handlers

--- pipelines/preprocess_raw_data/handler.py
+++ pipelines/preprocess_raw_data/handler.py
@@ -1,6 +1,8 @@
 import json
 import traceback
 
+from api.integrations_router.constants import dynamodb_table_name
+from lib.helper import generate_current_datetime_str
 from lib.log.logger import Logger
 from services.preprocess_raw_data.helper import preprocess_latest_raw_data
 
@@ -19,14 +21,21 @@ def lambda_handler(event: dict, context: dict):
         backfill_duration = event.get("backfill_duration", None)
         if backfill_duration is not None:
             backfill_duration = int(backfill_duration)
-        preprocess_latest_raw_data(
-            backfill_period=backfill_period, backfill_duration=backfill_duration
+        session_metadata: dict = preprocess_latest_raw_data(
+            backfill_period=backfill_period,
+            backfill_duration=backfill_duration,
+            event=event,
         )
         logger.info("Completed preprocessing pipeline in Lambda.")
-        return {
-            "statusCode": 200,
+        session_status_metadata = {
+            "service": "preprocess_raw_data",
+            "timestamp": session_metadata["preprocessing_timestamp"],
+            "status_code": 200,
             "body": json.dumps("Preprocessing completed successfully"),
+            "metadata_table_name": dynamodb_table_name,
+            "metadata": json.dumps(session_metadata),
         }
+        return session_status_metadata
     except Exception as e:
         logger.error(f"Error in preprocessing pipeline: {e}")
         logger.error(traceback.format_exc())
@@ -38,7 +47,15 @@ def lambda_handler(event: dict, context: dict):
                 }
             )
         )
-        raise
+        session_metadata = {
+            "service": "preprocess_raw_data",
+            "timestamp": generate_current_datetime_str(),
+            "status_code": 500,
+            "body": json.dumps(f"Error in preprocessing pipeline: {str(e)}"),
+            "metadata_table_name": dynamodb_table_name,
+            "metadata": json.dumps(traceback.format_exc()),
+        }
+        return session_metadata
 
 
 if __name__ == "__main__":
