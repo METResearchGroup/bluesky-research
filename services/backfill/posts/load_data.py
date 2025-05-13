a716e4980d6349b155f9809275b45d5076fc3c03
mark-torres10 <markptorres1@gmail.com>
2025-05-13 15:52:26 +0800

    Remove breakpoints

--- services/backfill/posts/load_data.py
+++ services/backfill/posts/load_data.py
@@ -197,8 +197,6 @@ def load_posts_to_backfill(
         output_format="list",
     )
 
-    breakpoint()
-
     posts_to_backfill_by_integration: dict[str, list[dict]] = {}
     for integration in valid_integrations:
         integration_post_uris: set[str] = load_service_post_uris(
@@ -209,7 +207,5 @@ def load_posts_to_backfill(
         posts_to_backfill_by_integration[integration] = [
             post for post in total_posts if post["uri"] not in integration_post_uris
         ]
-
-    breakpoint()
 
     return posts_to_backfill_by_integration
