996b263a4e9f9a5ff905c07facdb8b92b86d7d5e
mark-torres10 <43793907+mark-torres10@users.noreply.github.com>
2025-06-16 14:38:02 -0500

    Add valence classifications to agg label analytics (#177)
    * Add valence classifications to agg label analytics
    
    * Add fix for 'Get daily engagement metric statistics (we previously aggregated on week level)' ticket
    
    * Add daily records for aggregate engagement metrics
    
    * Add sorting + remove inplace=True
    
    * fix bug where days without engagement weren't being included in engagement metrics
    
    * Fix bug in analytics script. Add bash script for running valence classifier queue jobs

---
+++ pipelines/backfill_records_coordination/integration_specific_backfill/ml_inference_valence_classifier.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH -A p32375
+#SBATCH -p gengpu
+#SBATCH --gres=gpu:a100:2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH -t 1:00:00
+#SBATCH --mem=20G
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=markptorres1@gmail.com
+#SBATCH --job-name=backfill_records_coordination_ml_inference_valence_classifier_%j
+#SBATCH --output=/projects/p32375/bluesky-research/lib/log/backfill_records_coordination/ml_inference_valence_classifier-%j.log
+
+# load conda env
+CONDA_PATH="/hpc/software/mamba/23.1.0/etc/profile.d/conda.sh"
+
+# set pythonpath
+PYTHONPATH="/projects/p32375/bluesky-research/:$PYTHONPATH"
+
+# Ensure CUDA sees both GPUs
+export CUDA_VISIBLE_DEVICES=0,1
+
+source $CONDA_PATH && conda activate bluesky_research && export PYTHONPATH=$PYTHONPATH
+echo "Starting slurm job."
+echo "Number of available GPUs: $(nvidia-smi -L | wc -l)"
+nvidia-smi  # Print GPU info at start
+
+python "/projects/p32375/bluesky-research/pipelines/backfill_records_coordination/app.py" --record-type posts --integration ml_inference_valence_classifier --run-integrations
+exit_code=$?
+echo "Python script exited with code $exit_code"
+if [ $exit_code -ne 0 ]; then
+    echo "Job failed with exit code $exit_code"
+    exit $exit_code
+fi
+echo "Completed slurm job."
+
+# Send log file via email
+log_file="/projects/p32375/bluesky-research/lib/log/backfill_records_coordination/ml_inference_valence_classifier-${SLURM_JOB_ID}.log"
+if [ -f "$log_file" ]; then
+    mail -s "Log output for job ${SLURM_JOB_ID}" markptorres1@gmail.com < "$log_file"
+fi
+
+exit 0
