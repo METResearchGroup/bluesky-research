5b9bea8e5366688a619f0056b32f26208b3f7f52
mark-torres10 <markptorres1@gmail.com>
2025-08-26 21:54:05 -0500

    [Migration 2]: Set up basic file structure for migration of condition_aggregated.py

---
+++ services/calculate_analytics/analyses/user_feed_analysis_2025_04_08/main.py
@@ -0,0 +1,21 @@
+"""Calculate per-user, per-feed aggregate analyses.
+
+Refactored and migrated version of condition_aggregated.py
+"""
+
+
+def do_setup():
+    pass
+
+
+def do_aggregations_and_export_results():
+    pass
+
+
+def main():
+    do_setup()
+    do_aggregations_and_export_results()
+
+
+if __name__ == "__main__":
+    main()
