7af294d716ca2c76af80b74d3349f0c90bb4e96a
mark-torres10 <markptorres1@gmail.com>
2025-09-01 12:34:32 +0800

    [Migration 1 + 2] Update feed analyses main.py with tests

--- services/calculate_analytics/analyses/user_feed_analysis_2025_04_08/main.py
+++ services/calculate_analytics/analyses/user_feed_analysis_2025_04_08/main.py
@@ -8,6 +8,7 @@ import os
 import pandas as pd
 
 from lib.helper import generate_current_datetime_str, get_partition_dates
+from lib.log.logger import get_logger
 from services.calculate_analytics.shared.analysis.content_analysis import (
     get_daily_feed_content_per_user_metrics,
     get_weekly_content_per_user_metrics,
@@ -41,30 +42,44 @@ feed_content_weekly_aggregated_results_export_fp = os.path.join(
     f"weekly_feed_content_aggregated_results_per_user_{current_datetime_str}.csv",
 )
 
+logger = get_logger(__file__)
+
 
 def do_setup():
     """Setup steps for analysis."""
 
     # load users and partition dates.
-    user_df, user_date_to_week_df, valid_study_users_dids = load_user_data()
-    partition_dates: list[str] = get_partition_dates(
-        start_date=STUDY_START_DATE,
-        end_date=STUDY_END_DATE,
-        exclude_partition_dates=exclude_partition_dates,  # feed generation was broken on this date.
-    )
+    try:
+        user_df, user_date_to_week_df, valid_study_users_dids = load_user_data()
+        partition_dates: list[str] = get_partition_dates(
+            start_date=STUDY_START_DATE,
+            end_date=STUDY_END_DATE,
+            exclude_partition_dates=exclude_partition_dates,  # feed generation was broken on this date.
+        )
+    except Exception as e:
+        logger.error(f"Failed to load user data and/or partition dates: {e}")
+        raise
 
     # load feeds: per-user, per-date, get list of URIs of posts in feeds.
-    user_to_content_in_feeds: dict[str, dict[str, set[str]]] = get_feeds_per_user(
-        valid_study_users_dids=valid_study_users_dids
-    )
+    try:
+        user_to_content_in_feeds: dict[str, dict[str, set[str]]] = get_feeds_per_user(
+            valid_study_users_dids=valid_study_users_dids
+        )
+    except Exception as e:
+        logger.error(f"Failed to get feeds per user: {e}")
+        raise
 
     # load labels for feed content
-    feed_content_uris: set[str] = get_all_post_uris_used_in_feeds(
-        user_to_content_in_feeds=user_to_content_in_feeds
-    )
-    labels_for_feed_content: dict[str, dict] = get_all_labels_for_posts(
-        post_uris=feed_content_uris, partition_dates=partition_dates
-    )
+    try:
+        feed_content_uris: set[str] = get_all_post_uris_used_in_feeds(
+            user_to_content_in_feeds=user_to_content_in_feeds
+        )
+        labels_for_feed_content: dict[str, dict] = get_all_labels_for_posts(
+            post_uris=feed_content_uris, partition_dates=partition_dates
+        )
+    except Exception as e:
+        logger.error(f"Failed to get labels for feed content: {e}")
+        raise
 
     return {
         "user_df": user_df,
@@ -90,68 +105,92 @@ def do_aggregations_and_export_results(
     - (2) weekly.
     """
     # (1) Daily aggregations
-    print("[Daily analysis] Getting per-user, per-day content label metrics...")
+    logger.info("[Daily analysis] Getting per-user, per-day content label metrics...")
 
-    user_per_day_content_label_metrics: dict[
-        str, dict[str, dict[str, float | None]]
-    ] = get_daily_feed_content_per_user_metrics(
-        user_to_content_in_feeds=user_to_content_in_feeds,
-        labels_for_feed_content=labels_for_feed_content,
-    )
-
-    transformed_per_user_per_day_content_label_metrics: pd.DataFrame = (
-        transform_daily_content_per_user_metrics(
-            user_per_day_content_label_metrics=user_per_day_content_label_metrics,
-            users=user_df,
-            partition_dates=partition_dates,
+    try:
+        user_per_day_content_label_metrics: dict[
+            str, dict[str, dict[str, float | None]]
+        ] = get_daily_feed_content_per_user_metrics(
+            user_to_content_in_feeds=user_to_content_in_feeds,
+            labels_for_feed_content=labels_for_feed_content,
         )
-    )
+    except Exception as e:
+        logger.error(f"Failed to get daily feed content per user metrics: {e}")
+        raise
 
-    print(
-        f"[Daily analysis] Exporting per-user, per-day content label metrics to {feed_content_daily_aggregated_results_export_fp}..."
-    )
-    os.makedirs(
-        os.path.dirname(feed_content_daily_aggregated_results_export_fp),
-        exist_ok=True,
-    )
-    transformed_per_user_per_day_content_label_metrics.to_csv(
-        feed_content_daily_aggregated_results_export_fp, index=False
-    )
-    print(
-        f"[Daily analysis] Exported per-user, per-day content label metrics to {feed_content_daily_aggregated_results_export_fp}..."
-    )
+    try:
+        transformed_per_user_per_day_content_label_metrics: pd.DataFrame = (
+            transform_daily_content_per_user_metrics(
+                user_per_day_content_label_metrics=user_per_day_content_label_metrics,
+                users=user_df,
+                partition_dates=partition_dates,
+            )
+        )
+    except Exception as e:
+        logger.error(f"Failed to transform daily feed content per user metrics: {e}")
+        raise
+
+    try:
+        logger.info(
+            f"[Daily analysis] Exporting per-user, per-day content label metrics to {feed_content_daily_aggregated_results_export_fp}..."
+        )
+        os.makedirs(
+            os.path.dirname(feed_content_daily_aggregated_results_export_fp),
+            exist_ok=True,
+        )
+        transformed_per_user_per_day_content_label_metrics.to_csv(
+            feed_content_daily_aggregated_results_export_fp, index=False
+        )
+        logger.info(
+            f"[Daily analysis] Exported per-user, per-day content label metrics to {feed_content_daily_aggregated_results_export_fp}..."
+        )
+    except Exception as e:
+        logger.error(f"Failed to export daily feed content per user metrics: {e}")
+        raise
 
     # (2) Weekly aggregations.
-    print("[Weekly analysis] Getting per-user, per-week content label metrics...")
+    logger.info("[Weekly analysis] Getting per-user, per-week content label metrics...")
 
-    user_per_week_content_label_metrics: dict[
-        str, dict[str, dict[str, float | None]]
-    ] = get_weekly_content_per_user_metrics(
-        user_per_day_content_label_metrics=user_per_day_content_label_metrics,
-        user_date_to_week_df=user_date_to_week_df,
-    )
-
-    transformed_per_user_per_week_feed_content_metrics: pd.DataFrame = (
-        transform_weekly_content_per_user_metrics(
-            user_per_week_content_label_metrics=user_per_week_content_label_metrics,
-            users=user_df,
+    try:
+        user_per_week_content_label_metrics: dict[
+            str, dict[str, dict[str, float | None]]
+        ] = get_weekly_content_per_user_metrics(
+            user_per_day_content_label_metrics=user_per_day_content_label_metrics,
             user_date_to_week_df=user_date_to_week_df,
         )
-    )
+    except Exception as e:
+        logger.error(f"Failed to get weekly feed content per user metrics: {e}")
+        raise
 
-    print(
-        f"[Weekly analysis] Exporting per-user, per-week content label metrics to {feed_content_weekly_aggregated_results_export_fp}..."
-    )
-    os.makedirs(
-        os.path.dirname(feed_content_weekly_aggregated_results_export_fp),
-        exist_ok=True,
-    )
-    transformed_per_user_per_week_feed_content_metrics.to_csv(
-        feed_content_weekly_aggregated_results_export_fp, index=False
-    )
-    print(
-        f"[Weekly analysis] Exported per-user, per-week content label metrics to {feed_content_weekly_aggregated_results_export_fp}..."
-    )
+    try:
+        transformed_per_user_per_week_feed_content_metrics: pd.DataFrame = (
+            transform_weekly_content_per_user_metrics(
+                user_per_week_content_label_metrics=user_per_week_content_label_metrics,
+                users=user_df,
+                user_date_to_week_df=user_date_to_week_df,
+            )
+        )
+    except Exception as e:
+        logger.error(f"Failed to transform weekly feed content per user metrics: {e}")
+        raise
+
+    try:
+        logger.info(
+            f"[Weekly analysis] Exporting per-user, per-week content label metrics to {feed_content_weekly_aggregated_results_export_fp}..."
+        )
+        os.makedirs(
+            os.path.dirname(feed_content_weekly_aggregated_results_export_fp),
+            exist_ok=True,
+        )
+        transformed_per_user_per_week_feed_content_metrics.to_csv(
+            feed_content_weekly_aggregated_results_export_fp, index=False
+        )
+        logger.info(
+            f"[Weekly analysis] Exported per-user, per-week content label metrics to {feed_content_weekly_aggregated_results_export_fp}..."
+        )
+    except Exception as e:
+        logger.error(f"Failed to export weekly feed content per user metrics: {e}")
+        raise
 
 
 def main():
@@ -159,23 +198,31 @@ def main():
     appeared in users' feeds during the study, both at the daily and weekly
     aggregation levels.
     """
-    setup_objs = do_setup()
+    try:
+        setup_objs = do_setup()
 
-    user_df: pd.DataFrame = setup_objs["user_df"]
-    user_date_to_week_df: pd.DataFrame = setup_objs["user_date_to_week_df"]
-    user_to_content_in_feeds: dict[str, dict[str, set[str]]] = setup_objs[
-        "user_to_content_in_feeds"
-    ]
-    labels_for_feed_content: dict[str, dict] = setup_objs["labels_for_feed_content"]
-    partition_dates: list[str] = setup_objs["partition_dates"]
+        user_df: pd.DataFrame = setup_objs["user_df"]
+        user_date_to_week_df: pd.DataFrame = setup_objs["user_date_to_week_df"]
+        user_to_content_in_feeds: dict[str, dict[str, set[str]]] = setup_objs[
+            "user_to_content_in_feeds"
+        ]
+        labels_for_feed_content: dict[str, dict] = setup_objs["labels_for_feed_content"]
+        partition_dates: list[str] = setup_objs["partition_dates"]
+    except Exception as e:
+        logger.error(f"Failed to setup: {e}")
+        raise
 
-    do_aggregations_and_export_results(
-        user_df=user_df,
-        user_date_to_week_df=user_date_to_week_df,
-        user_to_content_in_feeds=user_to_content_in_feeds,
-        labels_for_feed_content=labels_for_feed_content,
-        partition_dates=partition_dates,
-    )
+    try:
+        do_aggregations_and_export_results(
+            user_df=user_df,
+            user_date_to_week_df=user_date_to_week_df,
+            user_to_content_in_feeds=user_to_content_in_feeds,
+            labels_for_feed_content=labels_for_feed_content,
+            partition_dates=partition_dates,
+        )
+    except Exception as e:
+        logger.error(f"Failed to do aggregations and export results: {e}")
+        raise
 
 
 if __name__ == "__main__":
