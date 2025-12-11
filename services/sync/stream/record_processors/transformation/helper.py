9bf70ebb2b4b9fa39f232128725186ae0f29e3ac
mark-torres10 <markptorres1@gmail.com>
2025-12-11 15:33:32 -0600

    Create a utility function to take care of record filename generation

--- services/sync/stream/record_processors/transformation/helper.py
+++ services/sync/stream/record_processors/transformation/helper.py
@@ -1,3 +1,6 @@
+from services.sync.stream.types import Operation, RecordType
+
+
 def extract_uri_suffix(uri: str) -> str:
     """Extract post URI suffix from full URI.
 
@@ -18,15 +21,30 @@ def extract_uri_suffix(uri: str) -> str:
     return uri.split("/")[-1]
 
 
-def build_record_filename(record_type: str, author_did: str, uri_suffix: str) -> str:
+def build_record_filename(
+    record_type: RecordType, operation: Operation, author_did: str, uri_suffix: str
+) -> str:
     """Build filename for record.
 
     Args:
         record_type: Record type
+        operation: Operation
         author_did: Author DID
         uri_suffix: URI suffix
 
     Returns:
-        Filename string
+        Filename string.
+
+    For CREATE operations, the filename is something like:
+        author_did=did:plc:abc123_post_uri_suffix=3kwd3wuubke2i.json (the record type and author DID are included)
+    For DELETE operations, the filename is something like:
+        post_uri_suffix=3kwd3wuubke2i.json (only the URI suffix is needed)
     """
-    return f"{record_type}_author_did={author_did}_uri_suffix={uri_suffix}.json"
+    if operation == Operation.CREATE:
+        return (
+            f"author_did={author_did}_{record_type.value}_uri_suffix={uri_suffix}.json"
+        )
+    elif operation == Operation.DELETE:
+        return f"uri_suffix={uri_suffix}.json"
+    else:
+        raise ValueError(f"Unknown operation: {operation}")
