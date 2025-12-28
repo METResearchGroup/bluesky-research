892da82730500df3ceac60f840fcec2840f6a8f6
mark-torres10 <markptorres1@gmail.com>
2025-12-28 13:40:16 -0500

    remove duplication in dataloader

--- services/consolidate_enrichment_integrations/load_data.py
+++ services/consolidate_enrichment_integrations/load_data.py
@@ -3,15 +3,10 @@
 from typing import Optional
 
 import pandas as pd
-from pydantic import ValidationError
 
-from lib.db.data_processing import parse_converted_pandas_dicts
 from lib.db.manage_local_data import load_data_from_local_storage
 from lib.log.logger import get_logger
 
-from services.consolidate_enrichment_integrations.models import (
-    ConsolidatedEnrichedPostModel,
-)
 
 logger = get_logger(__name__)
 
@@ -34,23 +29,7 @@ def load_enriched_posts(
     Raises:
         ValidationError: If any post fails schema validation.
     """
-    loaded_posts_df: pd.DataFrame = load_data_from_local_storage(
+    return load_data_from_local_storage(
         service="consolidated_enriched_post_records",
         latest_timestamp=latest_timestamp,
     )
-    df_dicts = loaded_posts_df.to_dict(orient="records")
-    df_dicts = parse_converted_pandas_dicts(df_dicts)
-
-    # Validate schemas of posts, then convert back to dataframe
-    try:
-        validated_post_models = [
-            ConsolidatedEnrichedPostModel(**post) for post in df_dicts
-        ]
-    except ValidationError as e:
-        logger.error(f"Failed to validate schemas of posts: {e}")
-        raise
-
-    posts_df: pd.DataFrame = pd.DataFrame(
-        [post.model_dump() for post in validated_post_models]
-    )
-    return posts_df
