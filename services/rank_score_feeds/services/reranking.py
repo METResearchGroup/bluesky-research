529ad516b43a1f42f23c922163d886cbe4395744
mark-torres10 <markptorres1@gmail.com>
2026-01-03 11:09:57 -0600

    refactor reranking service

--- services/rank_score_feeds/services/reranking.py
+++ services/rank_score_feeds/services/reranking.py
@@ -16,50 +16,97 @@ class RerankingService:
             feed_config: Configuration for feed generation algorithm.
         """
         self.config = feed_config
+        self.max_feed_length: int = self.config.max_feed_length
+        self.feed_preprocessing_multiplier: int = (
+            self.config.feed_preprocessing_multiplier
+        )
+        self.max_prop_old_posts: float = self.config.max_prop_old_posts
+        self.jitter_amount: int = self.config.jitter_amount
 
-    # TODO: refactor this.
     def rerank_feed(
         self,
         feed: list[CustomFeedPost],
         uris_of_posts_used_in_previous_feeds: set[str],
     ) -> list[CustomFeedPost]:
-        """Postprocesses the feed."""
-        # do feed postprocessing on a subset of the feed to save time.
-        max_feed_length: int = self.config.max_feed_length
-        feed_preprocessing_multiplier: int = self.config.feed_preprocessing_multiplier
-        max_prop_old_posts: float = self.config.max_prop_old_posts
-        jitter_amount: int = self.config.jitter_amount
+        """Postprocesses the feed using business rules and constraints."""
+        postprocessed_feed: list[CustomFeedPost] = (
+            self._clip_feed_to_preprocessing_window(feed=feed)
+        )
+        postprocessed_feed = self._enforce_fresh_content_in_feed(
+            feed=postprocessed_feed,
+            uris_of_posts_used_in_previous_feeds=uris_of_posts_used_in_previous_feeds,
+        )
+        postprocessed_feed = self._truncate_feed(
+            feed=postprocessed_feed,
+            max_feed_length=self.max_feed_length,
+        )
+        postprocessed_feed = self._jitter_feed(
+            feed=postprocessed_feed, jitter_amount=self.jitter_amount
+        )
+        self._validate_feed_length(
+            feed=postprocessed_feed, max_feed_length=self.max_feed_length
+        )
 
-        feed = feed[: int(max_feed_length * feed_preprocessing_multiplier)]
+        return postprocessed_feed
 
-        # ensure that there's a maximum % of old posts in the feed, so we
-        # always have some fresh content.
-        if uris_of_posts_used_in_previous_feeds:
-            max_num_old_posts = int(max_feed_length * max_prop_old_posts)
-            old_post_count = 0
-            processed_feed = []
-            for post in feed:
-                if post.item in uris_of_posts_used_in_previous_feeds:
-                    if old_post_count < max_num_old_posts:
-                        old_post_count += 1
-                        processed_feed.append(post)
-                else:
-                    processed_feed.append(post)
-            feed = processed_feed
+    def _clip_feed_to_preprocessing_window(
+        self, feed: list[CustomFeedPost]
+    ) -> list[CustomFeedPost]:
+        """Clips the feed to the preprocessing window.
 
-        # truncate feed
-        feed = feed[:max_feed_length]
+        We clip the feed to some multiple (>1) of the max feed length, so that
+        we can do postprocessing on a larger candidate set (but not the full feed,
+        as the end values of the feed tend to be lower value anyways)
+        before final truncation and business logic.
+        """
+        return feed[: int(self.max_feed_length * self.feed_preprocessing_multiplier)]
 
-        # jitter feed to slightly shuffle ordering
-        feed = self._jitter_feed(feed=feed, jitter_amount=jitter_amount)
+    def _enforce_fresh_content_in_feed(
+        self, feed: list[CustomFeedPost], uris_of_posts_used_in_previous_feeds: set[str]
+    ) -> list[CustomFeedPost]:
+        """Enforces fresh content in the feed.
 
-        # validate feed lengths:
-        if len(feed) != max_feed_length:
+        We ensure that there's a maximum % of old posts in the feed, so we
+        always have some fresh content.
+        """
+        if not uris_of_posts_used_in_previous_feeds:
+            return feed
+
+        max_num_old_posts = int(self.max_feed_length * self.max_prop_old_posts)
+        old_post_count = 0
+        processed_feed: list[CustomFeedPost] = []
+
+        for post in feed:
+            # Case 1: Always include fresh posts.
+            if post.item not in uris_of_posts_used_in_previous_feeds:
+                processed_feed.append(post)
+                continue
+
+            # Case 2: Include old posts up to the max limit.
+            if old_post_count < max_num_old_posts:
+                old_post_count += 1
+                processed_feed.append(post)
+
+        return processed_feed
+
+    def _truncate_feed(
+        self, feed: list[CustomFeedPost], max_feed_length: int
+    ) -> list[CustomFeedPost]:
+        """Truncates the feed to the max feed length."""
+        return feed[:max_feed_length]
+
+    def _validate_feed_length(
+        self, feed: list[CustomFeedPost], max_feed_length: int
+    ) -> None:
+        """Validates the feed.
+
+        Given the filtering, the max length is the max_feed_length, and we
+        want to be flagged if it's less than that (it can't be more than that).
+        """
+        if len(feed) < max_feed_length:
             raise ValueError(
-                f"Feed length is not equal to max_feed_length: {len(feed)} != {max_feed_length}"
+                f"Feed length is less than max_feed_length: {len(feed)} < {max_feed_length}"
             )
-
-        return feed
 
     def _jitter_feed(
         self, feed: list[CustomFeedPost], jitter_amount: int
