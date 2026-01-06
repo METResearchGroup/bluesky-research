768e2be3ff703c9bd7d7e8c23f0c4f2f074f6c49
mark-torres10 <43793907+mark-torres10@users.noreply.github.com>
2026-01-06 10:21:11 -0600

    Fix remaining incorrect S3 paths for Glue schemas (#280)
    * fix bug fix
    
    * fix bug fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -3615,7 +3615,7 @@ resource "aws_glue_catalog_table" "archive_fetch_posts_used_in_feeds" {
   }
 
   storage_descriptor {
-    location      = "s3://${var.s3_root_bucket_name}/bluesky_research/2024_nature_paper_study_data/fetch_posts_used_in_feeds/"
+    location      = "s3://${var.s3_root_bucket_name}/bluesky_research/2024_nature_paper_study_data/fetch_posts_used_in_feeds/cache/"
     input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
     output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
 
@@ -3717,7 +3717,7 @@ resource "aws_glue_catalog_table" "archive_in_network_user_activity" {
   }
 
   storage_descriptor {
-    location      = "s3://${var.s3_root_bucket_name}/bluesky_research/2024_nature_paper_study_data/in_network_user_activity/"
+    location      = "s3://${var.s3_root_bucket_name}/bluesky_research/2024_nature_paper_study_data/in_network_user_activity/cache/"
     input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
     output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
 
