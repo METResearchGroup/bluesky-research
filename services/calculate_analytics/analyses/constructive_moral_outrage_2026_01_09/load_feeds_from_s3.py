08d24aec5ed8531bba0ddfbed336271ec9dce85f
mark-torres10 <markptorres1@gmail.com>
2026-01-12 20:30:39 -0600

    move files to experiments

--- services/calculate_analytics/analyses/constructive_moral_outrage_2026_01_09/load_feeds_from_s3.py
+++
@@ -1,8 +0,0 @@
-"""Loads feeds from S3.
-
-Does it on a per-day basis, to avoid loading in too much data at once.
-"""
-
-# load all the partition dates
-# for each partition date, load the feeds
-# dump to parquet in (current dir)/feeds/(partition date).parquet
