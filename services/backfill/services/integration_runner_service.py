634c485708751ccd1208a48503f229a26d4cd353
mark-torres10 <43793907+mark-torres10@users.noreply.github.com>
2026-01-17 09:00:50 -0600

    Create `IntegrationRunnerService` implementation (#339)
    * wip: start integration runner service
    
    * introduce strategy pattern for delegating work across services
    
    * implement '_run_single_integration'
    
    * add unit tests

--- services/backfill/services/integration_runner_service.py
+++ services/backfill/services/integration_runner_service.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from lib.log.logger import get_logger
 from services.backfill.models import (
     IntegrationRunnerConfigurationPayload,
@@ -9,7 +10,63 @@ logger = get_logger(__file__)
 
 
 class IntegrationRunnerService:
+    def __init__(self):
+        self.integration_strategies = {}
+
+    def _load_integration_strategy(self, integration_name: str) -> Callable:
+        """Loads the integration strategy for the given integration name.
+
+        We directly return the classification functions because for the current
+        services, they have identical signatures for the classification functions.
+        If this changes, we would have a protocol for the classification functions
+        and consider using specific *Strategy classes. Right now, creating the Strategy
+        classes would be overkill.
+        """
+        if integration_name == "ml_inference_perspective_api":
+            from services.ml_inference.perspective_api.perspective_api import (
+                classify_latest_posts,
+            )
+
+            return classify_latest_posts
+        elif integration_name == "ml_inference_sociopolitical":
+            from services.ml_inference.sociopolitical.sociopolitical import (
+                classify_latest_posts,
+            )
+
+            return classify_latest_posts
+        elif integration_name == "ml_inference_ime":
+            from services.ml_inference.ime.ime import classify_latest_posts
+
+            return classify_latest_posts
+        elif integration_name == "ml_inference_valence_classifier":
+            from services.ml_inference.valence_classifier.valence_classifier import (
+                classify_latest_posts,
+            )
+
+            return classify_latest_posts
+        elif integration_name == "ml_inference_intergroup":
+            from services.ml_inference.intergroup.intergroup import (
+                classify_latest_posts,
+            )
+
+            return classify_latest_posts
+        else:
+            raise IntegrationRunnerServiceError(
+                f"Invalid integration name: {integration_name}"
+            )
+
+    def _get_or_load_integration_entrypoint(self, integration_name: str) -> Callable:
+        """Gets the integration entrypoint for the given integration name.
+        If the entrypoint is not found, it loads it from the integration strategies dictionary.
+        """
+        if integration_name not in self.integration_strategies:
+            self.integration_strategies[integration_name] = (
+                self._load_integration_strategy(integration_name)
+            )
+        return self.integration_strategies[integration_name]
+
     def run_integrations(self, payload: IntegrationRunnerServicePayload):
+        """Runs integrations for a list of integration configurations."""
         integration_configs = payload.integration_configs
         integration_names = [config.integration_name for config in integration_configs]
         total_integrations = len(integration_names)
@@ -36,4 +93,28 @@ class IntegrationRunnerService:
             ) from e
 
     def _run_single_integration(self, payload: IntegrationRunnerConfigurationPayload):
-        pass
+        """Runs a single integration."""
+        try:
+            dispatch_payload = self._create_integration_dispatch_payload(payload)
+            integration_fn = self._get_or_load_integration_entrypoint(
+                payload.integration_name
+            )
+            integration_fn(**dispatch_payload)
+        except Exception as e:
+            logger.error(f"Error running integration: {e}")
+            raise IntegrationRunnerServiceError(
+                f"Error running integration: {e}"
+            ) from e
+
+    def _create_integration_dispatch_payload(
+        self, payload: IntegrationRunnerConfigurationPayload
+    ) -> dict:
+        """Creates a dispatch payload matching the signature of the classification
+        functions."""
+        return {
+            "backfill_period": payload.backfill_period.value,
+            "backfill_duration": payload.backfill_duration,
+            "run_classification": True,  # TODO: need to deprecate run_classification. Here for backwards compatibility.
+            "previous_run_metadata": None,  # TODO: need to deprecate previous_run_metadata. Here for backwards compatibility.
+            "event": None,  # TODO: need to deprecate event. Here for backwards compatibility.
+        }
