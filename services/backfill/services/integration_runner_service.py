8a563de5843c3cef70d270dbc3a9ba56f93ce42a
mark-torres10 <markptorres1@gmail.com>
2026-01-17 08:31:56 -0600

    introduce strategy pattern for delegating work across services

--- services/backfill/services/integration_runner_service.py
+++ services/backfill/services/integration_runner_service.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from lib.log.logger import get_logger
 from services.backfill.models import (
     IntegrationRunnerConfigurationPayload,
@@ -9,6 +10,61 @@ logger = get_logger(__file__)
 
 
 class IntegrationRunnerService:
+    def __init__(self):
+        self.integration_strategies = {}
+
+    def _load_integration_strategy(self, integration_name: str) -> Callable:
+        """Loads the integration strategy for the given integration name.
+
+        We directly return the classification functions because for the current
+        services, they have identical signatures for the classification functions.
+        If this changes, we would have a protocol for the classification functions
+        and consider using specific *Strategy classes. Right now, creating the Strategy
+        classes would be overkill.
+        """
+        if integration_name == "ml_inference_perspective_api":
+            from services.ml_inference.perspective_api.perspective_api import (
+                classify_latest_posts,
+            )
+
+            return classify_latest_posts
+        elif integration_name == "ml_inference_sociopolitical":
+            from services.ml_inference.sociopolitical.sociopolitical import (
+                classify_latest_posts,
+            )
+
+            return classify_latest_posts
+        elif integration_name == "ml_inference_ime":
+            from services.ml_inference.ime.ime import classify_latest_posts
+
+            return classify_latest_posts
+        elif integration_name == "ml_inference_valence_classifier":
+            from services.ml_inference.valence_classifier.valence_classifier import (
+                classify_latest_posts,
+            )
+
+            return classify_latest_posts
+        elif integration_name == "ml_inference_intergroup":
+            from services.ml_inference.intergroup.intergroup import (
+                classify_latest_posts,
+            )
+
+            return classify_latest_posts
+        else:
+            raise IntegrationRunnerServiceError(
+                f"Invalid integration name: {integration_name}"
+            )
+
+    def _get_or_load_integration_strategy(self, integration_name: str) -> Callable:
+        """Gets the integration strategy for the given integration name.
+        If the strategy is not found, it loads it from the integration strategies dictionary.
+        """
+        if integration_name not in self.integration_strategies:
+            self.integration_strategies[integration_name] = (
+                self._load_integration_strategy(integration_name)
+            )
+        return self.integration_strategies[integration_name]
+
     def run_integrations(self, payload: IntegrationRunnerServicePayload):
         """Runs integrations for a list of integration configurations."""
         integration_configs = payload.integration_configs
