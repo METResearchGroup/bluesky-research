5d6a272e8aa3237324eca9f869dff6fd4bded114
Mark Torres <markptorres1@gmail.com>
2026-01-20 14:27:15 -0600

    add one-off script to create the credentials for accessing AWS with DuckDB

---
+++ lib/db/sql/init_duckdb_aws_access.py
@@ -0,0 +1,13 @@
+"""One-off script to run for enabling AWS access in DuckDB."""
+
+import duckdb
+
+con = duckdb.connect()
+con.execute("INSTALL httpfs; LOAD httpfs;")
+
+con.execute("""
+CREATE PERSISTENT SECRET aws_default_s3 (
+    TYPE s3,
+    PROVIDER credential_chain
+);
+""")
