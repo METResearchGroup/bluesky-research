ca8e98de98fedce47627e935587f227addfc9da0
mark-torres10 <markptorres1@gmail.com>
2026-01-22 16:56:59 -0600

    Add prompt-level batching for intergroup llm classifier

---
+++ services/ml_inference/intergroup/experiments/2026-01-22/prompts.py
@@ -0,0 +1,7 @@
+v1_prompt = ""
+
+batched_prompt = ""
+
+# maybe something about how the LLM will get an enumerated list of texts,
+# and it in turn has to return a JSON with the same number of items?
+# I'll have to see how this looks.
