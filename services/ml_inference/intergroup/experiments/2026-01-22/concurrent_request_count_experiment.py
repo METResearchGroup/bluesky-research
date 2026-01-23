fc6c60756f90bc5314c4d8e019a1f303173dbd8d
mark-torres10 <markptorres1@gmail.com>
2026-01-23 13:00:38 -0600

    wip: add naive concurrency experiment plus a batched concurrency experiment

--- services/ml_inference/intergroup/experiments/2026-01-22/concurrent_request_count_experiment.py
+++ services/ml_inference/intergroup/experiments/2026-01-22/concurrent_request_count_experiment.py
@@ -1,14 +1,6 @@
-"""Experimenting with concurrent request count.
-
-Now that we have an idea of the number of posts that we can put into a prompt
-before degradation (5), let's now explore the number of requests that we can do
-concurrently.
-
-We're interested in seeing, for a fixed number of posts 'n', the tradeoff between:
-
-1. (Our current approach): run N requests concurrently in parallel.
-2. (The proposed new approach): run N / 5 requests, each with a prompt of batch
-size 5, in parallel.
-
-Like before, we'll be comparing the runtime and accuracy of the two approaches.
+"""We're interested in just seeing the max number of concurrent
+requests that we can run without degradation, using the current
+approach.
 """
+
+pass
