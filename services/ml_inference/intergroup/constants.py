b6d2bd0b6d836c777d0d5fb7114fdafbc00787cc
mark-torres10 <markptorres1@gmail.com>
2026-01-23 19:34:33 -0600

    add note

--- services/ml_inference/intergroup/constants.py
+++ services/ml_inference/intergroup/constants.py
@@ -1,2 +1,7 @@
+# can basically increase this as much as we want,
+# as LiteLLM handles underlying concurrency management,
+# though it's around this point that we start seeing
+# linear to sublinear performance improvements
+# https://github.com/METResearchGroup/bluesky-research/pull/369
 DEFAULT_BATCH_SIZE = 500
 DEFAULT_LLM_MODEL_NAME = "gpt-5-nano"
