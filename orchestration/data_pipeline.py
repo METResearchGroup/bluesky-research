323222a7ef8dfa930a640edda0322e16505c7f63
mark-torres10 <markptorres1@gmail.com>
2026-05-14 11:02:16 -0500

    uncomment DAG. Add vector embedding DAG

--- orchestration/data_pipeline.py
+++ orchestration/data_pipeline.py
@@ -52,7 +52,7 @@ def run_ml_inference_perspective_api():
 
 @task(log_prints=True)
 def run_ml_inference_sociopolitical():
-    """Performs ML inference on the latest data using the Perspective API."""
+    """Performs ML inference on the latest data using the sociopolitical classifier."""
     bash_script_path = os.path.join(
         pipelines_directory, "classify_records", "sociopolitical", "submit_job.sh"
     )
@@ -98,17 +98,20 @@ def production_data_pipeline():
     job_run_ml_inference_perspective_api = run_ml_inference_perspective_api.submit(
         wait_for=[job_preprocess_raw_data]
     )
-    # job_run_ml_inference_sociopolitical = run_ml_inference_sociopolitical.submit(
-    #     wait_for=[job_preprocess_raw_data]
-    # )
-    # run_ml_inference_ime.submit(wait_for=[job_preprocess_raw_data]) # no need for consolidation to wait for this.
+    job_run_ml_inference_sociopolitical = run_ml_inference_sociopolitical.submit(
+        wait_for=[job_preprocess_raw_data]
+    )
+    job_run_ml_inference_ime = run_ml_inference_ime.submit(
+        wait_for=[job_preprocess_raw_data]
+    )
 
     # run enrichment integration consolidation after all integrations are finished.
     consolidate_enrichment_integrations.submit(
         wait_for=[
             job_calculate_superposters,
             job_run_ml_inference_perspective_api,
-            # job_run_ml_inference_sociopolitical,
+            job_run_ml_inference_sociopolitical,
+            job_run_ml_inference_ime,
         ]
     )
 
